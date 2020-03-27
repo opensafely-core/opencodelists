fe96f570e2cf70face19607c4f43fcf50e01f5f0
Peter Inglesby <peter.inglesby@gmail.com>
2020-03-27 20:39:35 +0000

    Add basic models and import script for CTV3

---
+++ ctv3/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Ctv3Config(AppConfig):
+    name = "ctv3"
---
+++ ctv3/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+
+class Concept(models.Model):
+    read_code = models.CharField(primary_key=True, max_length=5)
+    unknown_field_1 = models.CharField(max_length=1)
+    unknown_field_2 = models.CharField(max_length=1)
+    another_concept = models.ForeignKey("Concept", on_delete=models.CASCADE)
+    children = models.ManyToManyField(
+        "self",
+        through="ConceptHierarchy",
+        through_fields=("parent", "child"),
+        symmetrical=False,
+        related_name="parents",
+    )
+    terms = models.ManyToManyField(
+        "Term", through="ConceptTermMapping", related_name="concepts",
+    )
+
+
+class ConceptHierarchy(models.Model):
+    parent = models.ForeignKey(
+        "Concept", on_delete=models.CASCADE, related_name="parent_links"
+    )
+    child = models.ForeignKey(
+        "Concept", on_delete=models.CASCADE, related_name="child_links"
+    )
+    unknown_field_3 = models.CharField(max_length=2)
+
+
+class Term(models.Model):
+    term_id = models.CharField(primary_key=True, max_length=5)
+    unknown_field_4 = models.CharField(max_length=1)
+    name_1 = models.CharField(max_length=30)
+    name_2 = models.CharField(max_length=60, null=True)
+    name_3 = models.CharField(max_length=198, null=True)
+
+
+class ConceptTermMapping(models.Model):
+    concept = models.ForeignKey("Concept", on_delete=models.CASCADE)
+    term = models.ForeignKey("Term", on_delete=models.CASCADE)
+    unknown_field_5 = models.CharField(max_length=1)

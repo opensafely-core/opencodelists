e87b9f47a4ce318e8f92e31440ef7572ee4e9574
Peter Inglesby <peter.inglesby@gmail.com>
2020-03-27 23:47:51 +0000

    Add scripts for checking/fixing formatting

---
+++ check_formatting.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+declare -a cmds=(
+    "black --check ."
+    "flake8"
+    "isort --check-only --recursive **/*.py"
+)
+
+for cmd in "${cmds[@]}"; do
+  echo "$cmd"
+  eval "$cmd" || exit 1
+done
---
+++ fix_formatting.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+declare -a cmds=(
+    "black ."
+    "isort --recursive **/*.py"
+)
+
+for cmd in "${cmds[@]}"; do
+  echo "$cmd"
+  eval "$cmd"
+done

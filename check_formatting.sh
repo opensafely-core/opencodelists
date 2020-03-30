4a9e4aa431996738a136f2dc3754be8989216e9e
Peter Inglesby <peter.inglesby@gmail.com>
2020-03-30 09:14:15 +0100

    Set requirements for Python 3.5

--- check_formatting.sh
+++ check_formatting.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 declare -a cmds=(
-    "black --check ."
+    # "black --check ."
     "flake8"
     "isort --check-only --recursive **/*.py"
 )
--- fix_formatting.sh
+++ fix_formatting.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 declare -a cmds=(
-    "black ."
+    # "black ."
     "isort --recursive **/*.py"
 )
 

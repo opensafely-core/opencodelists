7116e455ca309565e58c41e36f5bdb0a554ca428
Peter Inglesby <peter.inglesby@gmail.com>
2020-04-27 17:19:56 +0100

    Remove print

--- codelists/coding_systems.py
+++ codelists/coding_systems.py
@@ -10,6 +10,5 @@ for path in glob.glob(
     os.path.join(settings.BASE_DIR, "coding_systems", "*", "coding_system.py")
 ):
     coding_system_id = path.split(os.path.sep)[-2]
-    print(coding_system_id)
     mod = import_module(f"coding_systems.{coding_system_id}.coding_system")
     CODING_SYSTEMS[coding_system_id] = mod.CodingSystem

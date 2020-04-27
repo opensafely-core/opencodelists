82d1ed1b3e6f56cd6e1bd5a54ac5ae65cf828c30
Peter Inglesby <peter.inglesby@gmail.com>
2020-04-27 17:18:35 +0100

    Add with_environment.sh

---
+++ with_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+REPO_ROOT=$(dirname $0)
+VIRTUALENV_PATH=$REPO_ROOT/venv/bin
+
+source "$VIRTUALENV_PATH/activate"
+
+export PYTHONPATH="$PYTHONPATH:$REPO_ROOT"
+export DJANGO_SETTINGS_MODULE="opencodelists.settings"
+
+exec "$@"

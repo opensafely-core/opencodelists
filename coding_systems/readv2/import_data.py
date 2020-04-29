cf052bf1e709c4aaa86ee20c311746d9f2176ff5
Peter Inglesby <peter.inglesby@gmail.com>
2020-04-29 13:25:42 +0100

    Import readv2 data from proper release
    We were previously importing it from the data files of the NHS Read Code
    Browser, which had already been processed and (possibly... nothing was
    documented) included some non-V2 codes.

--- coding_systems/readv2/import_data.py
+++ coding_systems/readv2/import_data.py
@@ -1,43 +1,10 @@
+import csv
 import os
 
-from dbfread import DBF
-
-from .models import Concept, ConceptHierarchy, ConceptTermMapping, Term
+from .models import Concept
 
 
 def import_data(release_dir):
-    def load_records(filename):
-        path = os.path.join(release_dir, "Standard", "V2", filename)
-        yield from DBF(path, encoding="latin1", lowernames=True)
-
-    Concept.objects.bulk_create(
-        Concept(read_code=r["readcode"]) for r in load_records("CONCEPT.DBF")
-    )
-
-    ConceptHierarchy.objects.bulk_create(
-        ConceptHierarchy(child_id=r["cc"], parent_id=r["pcc"])
-        for r in load_records("HIER.DBF")
-    )
-
-    term_id_to_long_term_name = {
-        r["termid"]: r["term198"] for r in load_records("TERM198.DBF")
-    }
-    term_records = [
-        dict(**r, **{"term198": term_id_to_long_term_name.get("termid", "")})
-        for r in (load_records("TERM.DBF"))
-    ]
-
-    Term.objects.bulk_create(
-        Term(
-            term_id=r["termid"],
-            name_1=r["term30"],
-            name_2=r["term60"],
-            name_3=r["term198"],
-        )
-        for r in term_records
-    )
-
-    ConceptTermMapping.objects.bulk_create(
-        ConceptTermMapping(concept_id=r["cc"], term_id=r["termid"])
-        for r in load_records("DESC.DBF")
-    )
+    path = os.path.join(release_dir, "V2", "Unified", "Corev2.all")
+    with open(path) as f:
+        Concept.objects.bulk_create(Concept(*row) for row in csv.reader(f))
--- coding_systems/readv2/models.py
+++ coding_systems/readv2/models.py
@@ -3,41 +3,15 @@ from django.db import models
 
 class Concept(models.Model):
     read_code = models.CharField(primary_key=True, max_length=5)
-    children = models.ManyToManyField(
-        "self",
-        through="ConceptHierarchy",
-        through_fields=("parent", "child"),
-        symmetrical=False,
-        related_name="parents",
-    )
-    terms = models.ManyToManyField(
-        "Term", through="ConceptTermMapping", related_name="concepts",
-    )
-
-    def all_terms(self):
-        assert "terms" in self._prefetched_objects_cache
-        return self.terms.all()
-
-
-class ConceptHierarchy(models.Model):
-    parent = models.ForeignKey(
-        "Concept", on_delete=models.CASCADE, related_name="child_links"
-    )
-    child = models.ForeignKey(
-        "Concept", on_delete=models.CASCADE, related_name="parent_links"
-    )
-
-
-class Term(models.Model):
-    term_id = models.CharField(primary_key=True, max_length=5)
     name_1 = models.CharField(max_length=30)
-    name_2 = models.CharField(max_length=60, null=True)
-    name_3 = models.CharField(max_length=198, null=True)
-
-    def name(self):
-        return self.name_3 or self.name_2 or self.name_1
-
-
-class ConceptTermMapping(models.Model):
-    concept = models.ForeignKey("Concept", on_delete=models.CASCADE)
-    term = models.ForeignKey("Term", on_delete=models.CASCADE)
+    name_2 = models.CharField(max_length=60)
+    name_3 = models.CharField(max_length=198)
+    unknown_field_4 = models.CharField(max_length=15)  # Probably ICD9_CODE
+    unknown_field_5 = models.CharField(max_length=2)  # Probably ICD9_CODE_DEF
+    unknown_field_6 = models.CharField(max_length=13)  # Probably ICD9_CM_CODE
+    unknown_field_7 = models.CharField(max_length=2)  # Probably ICD9_CM_CODE_DEF
+    unknown_field_8 = models.CharField(max_length=19)  # Probably OPSC_CODE
+    unknown_field_9 = models.CharField(max_length=2)  # Probably OPSC_CODE_DEF
+    speciality_flags = models.CharField(max_length=10)
+    status = models.CharField(max_length=1)
+    language = models.CharField(max_length=2)

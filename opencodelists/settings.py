66a82b76197ebf671bd1dc13a756f0de3c55ead0
Peter Inglesby <peter.inglesby@gmail.com>
2020-05-19 13:06:00 +0100

    Use pysqlite3 instead of sqlite3

--- opencodelists/settings.py
+++ opencodelists/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+
 import os
+import sys
+
+# Patch sqlite3 to ensure recent version
+__import__("pysqlite3")
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

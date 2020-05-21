a96a80433e93cfcab6138b569d33e476b2144c1e
Peter Inglesby <peter.inglesby@gmail.com>
2020-05-21 16:38:31 +0100

    Reimplement read_code_to_snomedct_concepts

--- mappings/ctv3sctmap2/mappers.py
+++ mappings/ctv3sctmap2/mappers.py
@@ -1,15 +1,12 @@
-from coding_systems.snomedct.models import Concept as SnomedCTConcept
-
 from .models import Mapping
 
 
-def read_codes_to_snomedct_concepts(read_codes):
-    mappings = Mapping.objects.filter(ctv3_concept__in=read_codes)
-    mapped_read_codes = {m.ctv3_concept_id for m in mappings}
-    unmapped_read_codes = [rc for rc in read_codes if rc not in mapped_read_codes]
-
-    snomedct_concepts = SnomedCTConcept.objects.filter(
-        ctv3_mappings__in=mappings
-    ).distinct()
-
-    return snomedct_concepts, unmapped_read_codes
+def read_code_to_snomedct_concepts(read_codes):
+    # We don't use a defaultdict, because we want to know which codes haven't
+    # been mapped.
+    mapper = {read_code: [] for read_code in read_codes}
+    for mapping in Mapping.objects.filter(ctv3_concept__in=read_codes).select_related(
+        "sct_concept"
+    ):
+        mapper[mapping.ctv3_concept_id].append(mapping.sct_concept)
+    return mapper

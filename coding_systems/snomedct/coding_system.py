8151aee088235f4315406eda5728cfb69fe9789f
Peter Inglesby <peter.inglesby@gmail.com>
2020-05-21 16:37:55 +0100

    Add tree functions to snomedct.coding_system

--- coding_systems/snomedct/coding_system.py
+++ coding_systems/snomedct/coding_system.py
@@ -1,2 +1,68 @@
+from opencodelists.db_utils import query
+
+from .models import IS_A
+
 name = "SNOMED CT"
 short_name = "SNOMED CT"
+
+root = "138875005"
+
+
+def ancestor_relationships(codes):
+    placeholders = ", ".join(["%s"] * len(codes))
+    sql = f"""
+    WITH RECURSIVE tree(parent_id, child_id) AS (
+      SELECT
+        destination_id AS parent_id,
+        source_id AS child_id
+      FROM snomedct_relationship
+      WHERE child_id IN ({placeholders})
+        AND type_id = '{IS_A}'
+        AND active
+
+      UNION
+
+      SELECT
+        r.destination_id AS parent_id,
+        r.source_id AS child_id
+      FROM snomedct_relationship r
+      INNER JOIN tree t
+        ON r.source_id = t.parent_id
+      WHERE r.type_id = '{IS_A}'
+        AND active
+    )
+
+    SELECT parent_id, child_id FROM tree
+    """
+
+    return query(sql, codes)
+
+
+def descendant_relationships(codes):
+    placeholders = ", ".join(["%s"] * len(codes))
+    sql = f"""
+    WITH RECURSIVE tree(parent_id, child_id) AS (
+      SELECT
+        destination_id AS parent_id,
+        source_id AS child_id
+      FROM snomedct_relationship
+      WHERE parent_id IN ({placeholders})
+        AND type_id = '{IS_A}'
+        AND active
+
+      UNION
+
+      SELECT
+        r.destination_id AS parent_id,
+        r.source_id AS child_id
+      FROM snomedct_relationship r
+      INNER JOIN tree t
+        ON r.destination_id = t.child_id
+      WHERE r.type_id = '{IS_A}'
+        AND active
+    )
+
+    SELECT parent_id, child_id FROM tree
+    """
+
+    return query(sql, codes)

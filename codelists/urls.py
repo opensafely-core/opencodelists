25cbb7b078036b28e0fe41471c27f1f7c59eaaed
Peter Inglesby <peter.inglesby@gmail.com>
2020-07-15 12:15:28 +0100

    Add redirects for misspelled codelists

--- codelists/urls.py
+++ codelists/urls.py
@@ -1,10 +1,25 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from . import views
 
 app_name = "codelists"
 
 urlpatterns = [
+    # There was a typo in the codelist names.  If this happens a lot, we could
+    # add a "formerly known as" field to Codelist.
+    path(
+        "codelist/opensafely/permanent-immunosuppresion/",
+        RedirectView.as_view(
+            url="/codelist/opensafely/permanent-immunosuppression/", permanent=True
+        ),
+    ),
+    path(
+        "codelist/opensafely/temporary-immunosuppresion/",
+        RedirectView.as_view(
+            url="/codelist/opensafely/temporary-immunosuppression/", permanent=True
+        ),
+    ),
     path("", views.index, name="index"),
     path("codelist/<project_slug>/<codelist_slug>/", views.codelist, name="codelist"),
     path(

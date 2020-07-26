5b39234d09be76c603e4715727b61ae5df415137
Peter Inglesby <peter.inglesby@gmail.com>
2020-07-26 17:44:56 +0100

    Add actions and tests for codelists

---
+++ codelists/tests/factories.py
@@ -0,0 +1,25 @@
+from codelists import actions
+from opencodelists.tests.factories import create_project
+
+
+def create_codelist():
+    p = create_project()
+    return actions.create_codelist(
+        project=p,
+        name="Test Codelist",
+        coding_system_id="snomedct",
+        description="This is a test",
+        methodology="This is how we did it",
+        csv_data="code,description\n1067731000000107,Injury whilst swimming (disorder)",
+    )
+
+
+def create_draft_version():
+    cl = create_codelist()
+    return cl.versions.get()
+
+
+def create_published_version():
+    clv = create_draft_version()
+    actions.publish_version(version=clv)
+    return clv
---
+++ codelists/tests/test_actions.py
@@ -0,0 +1,85 @@
+import pytest
+
+from codelists import actions
+
+from . import factories
+
+pytestmark = pytest.mark.freeze_time("2020-07-23")
+
+
+def test_create_codelist():
+    p = factories.create_project()
+    cl = actions.create_codelist(
+        project=p,
+        name="Test Codelist",
+        coding_system_id="snomedct",
+        description="This is a test",
+        methodology="This is how we did it",
+        csv_data="code,description\n1067731000000107,Injury whilst swimming (disorder)",
+    )
+    assert cl.project == p
+    assert cl.name == "Test Codelist"
+    assert cl.slug == "test-codelist"
+    assert cl.coding_system_id == "snomedct"
+    assert cl.description == "This is a test"
+    assert cl.methodology == "This is how we did it"
+    assert cl.versions.count() == 1
+    clv = cl.versions.get()
+    assert clv.version_str == "2020-07-23"
+    assert "whilst swimming" in clv.csv_data
+
+
+def test_create_version_on_same_day():
+    cl = factories.create_codelist()
+    clv = actions.create_version(
+        codelist=cl,
+        csv_data="code,description\n1068181000000106, Injury whilst synchronised swimming (disorder)",
+    )
+    assert clv.version_str == "2020-07-23-a"
+    assert "whilst synchronised swimming" in clv.csv_data
+    assert cl.versions.count() == 2
+
+
+def test_create_version_on_next_day(freezer):
+    cl = factories.create_codelist()
+    freezer.move_to("2020-07-24")
+    clv = actions.create_version(
+        codelist=cl,
+        csv_data="code,description\n1068181000000106, Injury whilst synchronised swimming (disorder)",
+    )
+    assert clv.version_str == "2020-07-24"
+    assert "whilst synchronised swimming" in clv.csv_data
+    assert cl.versions.count() == 2
+
+
+def test_update_draft_version():
+    clv = factories.create_draft_version()
+    actions.update_version(
+        version=clv,
+        csv_data="code,description\n1068181000000106, Injury whilst synchronised swimming (disorder)",
+    )
+    clv.refresh_from_db()
+    assert "whilst synchronised swimming" in clv.csv_data
+    assert clv.codelist.versions.count() == 1
+
+
+def test_update_published_version():
+    clv = factories.create_published_version()
+    with pytest.raises(AssertionError):
+        actions.update_version(
+            version=clv,
+            csv_data="code,description\n1068181000000106, Injury whilst synchronised swimming (disorder)",
+        )
+
+
+def test_publish_draft_version():
+    clv = factories.create_draft_version()
+    actions.publish_version(version=clv)
+    clv.refresh_from_db()
+    assert not clv.is_draft
+
+
+def test_publish_published_version():
+    clv = factories.create_published_version()
+    with pytest.raises(AssertionError):
+        actions.publish_version(version=clv)

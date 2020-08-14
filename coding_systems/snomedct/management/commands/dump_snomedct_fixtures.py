86f6c65600ebc2f2fe312fb429e943da895db104
Peter Inglesby <peter.inglesby@gmail.com>
2020-08-14 12:36:29 +0100

    Update SNOMED CT fixtures

--- coding_systems/snomedct/management/commands/dump_snomedct_fixtures.py
+++ coding_systems/snomedct/management/commands/dump_snomedct_fixtures.py
@@ -1,17 +1,31 @@
+"""
+Generate a JSON fixture containing every Concept related to the given Concepts via an
+IS_A relationship.  Additionally includes every Description and IS_A Relationship for
+these Concepts.
+
+The generated fixture can be loaded with `loaddata`.
+"""
 from django.core import serializers
 from django.core.management import BaseCommand
 
-from ...models import CORE_METADATA_CONCEPT, IS_A, Concept, Description
+from ...models import IS_A, Concept, Description
 
 
 class Command(BaseCommand):
-    def handle(self, **kwargs):
-        concepts = set()
-        relationships = []
+    help = __doc__
 
-        core_metadata_concept = Concept.objects.get(pk=CORE_METADATA_CONCEPT)
-        is_a = Concept.objects.get(pk=IS_A)
-        todo = [core_metadata_concept]
+    def add_arguments(self, parser):
+        parser.add_argument("codes", nargs="+", help="Codes of concepts to start from")
+        parser.add_argument("--path", help="Path to write fixture to")
+
+    def handle(self, path, codes, **kwargs):
+        starting_concepts = Concept.objects.filter(pk__in=codes)
+        assert len(codes) == starting_concepts.count()
+        concepts = set()
+        relationships = set()
+
+        # Walk up the hierarchy, collecting concepts and relationships
+        todo = list(starting_concepts)
 
         while todo:
             concept = todo.pop()
@@ -21,22 +35,24 @@ class Command(BaseCommand):
             for rel in concept.source_relationships.filter(type_id=IS_A).select_related(
                 "source"
             ):
-                relationships.append(rel)
+                relationships.add(rel)
 
                 c = rel.source
                 if c not in concepts:
                     todo.append(c)
 
-        todo = [core_metadata_concept, is_a]
+        # Walk down the hierarchy
+        todo = list(starting_concepts)
 
         while todo:
             concept = todo.pop()
+            print(concept.id, concept.fully_specified_name)
             concepts.add(concept)
 
             for rel in concept.destination_relationships.filter(
                 type_id=IS_A
             ).select_related("destination"):
-                relationships.append(rel)
+                relationships.add(rel)
 
                 c = rel.destination
                 if c not in concepts:
@@ -44,7 +60,11 @@ class Command(BaseCommand):
 
         descriptions = Description.objects.filter(concept__in=concepts)
 
-        records = list(concepts) + relationships + list(descriptions)
+        print(f"{len(concepts)} concepts")
+        print(f"{len(relationships)} relationships")
+        print(f"{len(descriptions)} descriptions")
 
-        with open("coding_systems/snomedct/fixtures/snomedct.json", "w") as f:
+        records = list(concepts) + list(relationships) + list(descriptions)
+
+        with open(path, "w") as f:
             serializers.serialize("json", records, stream=f, indent=2)

f6184512be4dae921b5fbb682258c376565d647b
Peter Inglesby <peter.inglesby@gmail.com>
2020-08-20 09:16:23 +0100

    Use pytest in test_tree_utils

--- codelists/tests/test_tree_utils.py
+++ codelists/tests/test_tree_utils.py
@@ -1,38 +1,32 @@
-from unittest import TestCase
-
 from codelists import tree_utils
 
 from .helpers import build_tree
 
 
-class TreeUtilsTests(TestCase):
-    def test_walk_tree_depth_first(self):
-        tree = build_tree(depth=3)
+def test_walk_tree_depth_first():
+    tree = build_tree(depth=3)
 
-        # tree has this structure:
-        #
-        #     ┌---1---┐
-        #     |       |
-        #   ┌-2-┐   ┌-3-┐
-        #   |   |   |   |
-        #   4   5   6   7
+    # tree has this structure:
+    #
+    #     ┌---1---┐
+    #     |       |
+    #   ┌-2-┐   ┌-3-┐
+    #   |   |   |   |
+    #   4   5   6   7
 
-        self.assertEqual(
-            list(tree_utils.walk_tree_depth_first(tree)),
-            [
-                ("1", 1),
-                ("2", 1),
-                ("4", 1),
-                ("4", -1),
-                ("5", 1),
-                ("5", -1),
-                ("2", -1),
-                ("3", 1),
-                ("6", 1),
-                ("6", -1),
-                ("7", 1),
-                ("7", -1),
-                ("3", -1),
-                ("1", -1),
-            ],
-        )
+    assert list(tree_utils.walk_tree_depth_first(tree)) == [
+        ("1", 1),
+        ("2", 1),
+        ("4", 1),
+        ("4", -1),
+        ("5", 1),
+        ("5", -1),
+        ("2", -1),
+        ("3", 1),
+        ("6", 1),
+        ("6", -1),
+        ("7", 1),
+        ("7", -1),
+        ("3", -1),
+        ("1", -1),
+    ]

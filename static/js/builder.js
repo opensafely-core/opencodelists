7f5559ce2ec6188086ff68ff801f671b1f32c019
Peter Inglesby <peter.inglesby@gmail.com>
2020-08-20 14:02:45 +0100

    Break up CodelistBuilder

--- static/js/builder.js
+++ static/js/builder.js
@@ -43,6 +43,9 @@ class CodelistBuilder extends React.Component {
 
     this.updateStatus = this.updateStatus.bind(this);
     this.toggleVisibility = this.toggleVisibility.bind(this);
+    this.getStatus = this.getStatus.bind(this);
+    this.getIsExpanded = this.getIsExpanded.bind(this);
+    this.getIsVisible = this.getIsVisible.bind(this);
   }
 
   updateStatus(code, status) {
@@ -62,8 +65,6 @@ class CodelistBuilder extends React.Component {
 
       included = new Set(included);
       excluded = new Set(excluded);
-      console.log(included);
-      console.log(excluded);
 
       function newStatus(code) {
         // This function duplicates the logic of codelists.tree_utils.render
@@ -175,10 +176,6 @@ class CodelistBuilder extends React.Component {
     }));
   }
 
-  status(code) {
-    return this.state["status-" + code];
-  }
-
   isDirectlyRelated(code1, code2) {
     return (
       code1 === code2 ||
@@ -187,28 +184,24 @@ class CodelistBuilder extends React.Component {
     );
   }
 
-  isVisible(code) {
-    return ANCESTORS_MAP[code].every((ancestor) => this.isExpanded(ancestor));
+  getStatus(code) {
+    return this.state["status-" + code];
   }
 
-  isExpanded(code) {
+  getIsVisible(code) {
+    return ANCESTORS_MAP[code].every((ancestor) =>
+      this.getIsExpanded(ancestor)
+    );
+  }
+
+  getIsExpanded(code) {
     return this.state["expanded-" + code];
-  }
-
-  termStyle(code) {
-    return {
-      color: {
-        "!": "red",
-        "-": "gray",
-        "(-)": "gray",
-      }[this.status(code)],
-    };
   }
 
   counts() {
     let counts = { "?": 0, "!": 0, "+": 0, "(+)": 0, "-": 0, "(-)": 0 };
     CODES.forEach((code) => {
-      counts[this.status(code)] += 1;
+      counts[this.getStatus(code)] += 1;
     });
     counts["total"] = Object.values(counts).reduce((a, b) => a + b);
     return counts;
@@ -219,96 +212,35 @@ class CodelistBuilder extends React.Component {
       <div className="row">
         <div className="col-3">
           <h3 className="mb-4">Summary</h3>
-          {FILTER ? (
-            <p>Filtered to {FILTER} concepts and their descendants.</p>
-          ) : null}
+          <Filter />
           <Summary counts={this.counts()} />
           <hr />
+
           <h3 className="mb-4">Term searches</h3>
           <ul className="list-group">
             {SEARCHES.map((search) => (
-              <a
-                key={search.url}
-                href={search.url}
-                className={
-                  search.active
-                    ? "list-group-item list-group-item-action active"
-                    : "list-group-item list-group-item-action"
-                }
-              >
-                {search.term}
-              </a>
+              <TermSearch key={search.url} search={search} />
             ))}
           </ul>
           <hr />
+
           <h3 className="mb-4">New term search</h3>
-          <form method="post" action={SEARCH_URL}>
-            <div className="form-group">
-              <input
-                type="hidden"
-                name="csrfmiddlewaretoken"
-                value={getCookie("csrftoken")}
-              />
-              <input
-                type="search"
-                className="form-control"
-                name="term"
-                placeholder="Search term"
-              />
-            </div>
-            <button type="submit" name="search" className="btn btn-primary">
-              Search
-            </button>
-          </form>
+          <SearchForm />
         </div>
 
         <div className="col-9 pl-5">
           <h3 className="mb-4">Results</h3>
           {TABLES.map((table) => (
-            <div key={table.heading} className="mb-4">
-              <h4>{table.heading}</h4>
-              {table.rows.map((row, ix) => (
-                <div
-                  className="row"
-                  key={ix}
-                  style={{
-                    display: this.isVisible(row.code) ? "flex" : "none",
-                  }}
-                >
-                  <div className="btn-group btn-group-sm" role="group">
-                    <Button
-                      code={row.code}
-                      symbol="+"
-                      status={this.status(row.code)}
-                      isEditable={this.state.isEditable}
-                      handleClick={this.updateStatus}
-                    />
-                    <Button
-                      code={row.code}
-                      symbol="-"
-                      status={this.status(row.code)}
-                      isEditable={this.state.isEditable}
-                      handleClick={this.updateStatus}
-                    />
-                  </div>
-                  <div style={{ paddingLeft: row.indent + "em" }}>
-                    {DESCENDANTS_MAP[row.code].length ? (
-                      <span
-                        onClick={this.toggleVisibility.bind(null, row.code)}
-                        style={{ cursor: "pointer" }}
-                      >
-                        {this.isExpanded(row.code) ? "⊟" : "⊞"}
-                      </span>
-                    ) : null}
-                    <span style={this.termStyle(row.code)}>{row.term}</span>
-                    &nbsp;
-                    <span>
-                      (<code>{row.code}</code>)
-                    </span>
-                  </div>
-                </div>
-              ))}
-            </div>
+            <Table
+              key={table.heading}
+              table={table}
+              getStatus={this.getStatus}
+              getIsVisible={this.getIsVisible}
+              getIsExpanded={this.getIsExpanded}
+              isEditable={this.state.isEditable}
+              updateStatus={this.updateStatus}
+              toggleVisibility={this.toggleVisibility}
+            />
           ))}
         </div>
       </div>
@@ -316,11 +248,134 @@ class CodelistBuilder extends React.Component {
   }
 }
 
+function Filter(props) {
+  return FILTER ? (
+    <p>Filtered to {FILTER} concepts and their descendants.</p>
+  ) : null;
+}
+
+function TermSearch(props) {
+  const { search } = props;
+
+  return (
+    <a
+      href={search.url}
+      className={
+        search.active
+          ? "list-group-item list-group-item-action active"
+          : "list-group-item list-group-item-action"
+      }
+    >
+      {search.term}
+    </a>
+  );
+}
+
+function SearchForm(props) {
+  return (
+    <form method="post" action={SEARCH_URL}>
+      <div className="form-group">
+        <input
+          type="hidden"
+          name="csrfmiddlewaretoken"
+          value={getCookie("csrftoken")}
+        />
+        <input
+          type="search"
+          className="form-control"
+          name="term"
+          placeholder="Search term"
+        />
+      </div>
+      <button type="submit" name="search" className="btn btn-primary">
+        Search
+      </button>
+    </form>
+  );
+}
+
+function Table(props) {
+  const {
+    table,
+    getStatus,
+    getIsVisible,
+    getIsExpanded,
+    isEditable,
+    updateStatus,
+    toggleVisibility,
+  } = props;
+
+  return (
+    <div className="mb-4">
+      <h4>{table.heading}</h4>
+      {table.rows.map((row, ix) => (
+        <Row
+          key={ix}
+          row={row}
+          status={getStatus(row.code)}
+          isVisible={getIsVisible(row.code)}
+          isExpanded={getIsExpanded(row.code)}
+          isEditable={isEditable}
+          updateStatus={updateStatus}
+          toggleVisibility={toggleVisibility}
+        />
+      ))}
+    </div>
+  );
+}
+
+function Row(props) {
+  const {
+    row,
+    ix,
+    status,
+    isVisible,
+    isExpanded,
+    isEditable,
+    updateStatus,
+    toggleVisibility,
+  } = props;
+
+  const style = { display: isVisible ? "flex" : "none" };
+
+  return (
+    <div className="row" style={style}>
+      <div className="btn-group btn-group-sm" role="group">
+        <Button
+          code={row.code}
+          symbol="+"
+          status={status}
+          isEditable={isEditable}
+          updateStatus={updateStatus}
+        />
+        <Button
+          code={row.code}
+          symbol="-"
+          status={status}
+          isEditable={isEditable}
+          updateStatus={updateStatus}
+        />
+      </div>
+
+      <TermAndCode
+        term={row.term}
+        code={row.code}
+        depth={row.depth}
+        status={status}
+        isExpanded={isExpanded}
+        toggleVisibility={toggleVisibility}
+      />
+    </div>
+  );
+}
+
 function Button(props) {
+  const { code, symbol, status, isEditable, updateStatus } = props;
+
   let buttonClasses = ["btn"];
-  if (props.status === props.symbol) {
+  if (status === symbol) {
     buttonClasses.push("btn-primary");
-  } else if (props.status === `(${props.symbol})`) {
+  } else if (status === `(${symbol})`) {
     buttonClasses.push("btn-secondary");
   } else {
     buttonClasses.push("btn-outline-secondary");
@@ -331,14 +386,41 @@ function Button(props) {
   return (
     <button
       type="button"
-      onClick={
-        props.isEditable &&
-        props.handleClick.bind(null, props.code, props.symbol)
-      }
+      onClick={isEditable && updateStatus.bind(null, code, symbol)}
       className={buttonClasses.join(" ")}
     >
-      {props.symbol}
+      {symbol}
     </button>
+  );
+}
+
+function TermAndCode(props) {
+  const { term, code, depth, status, isExpanded, toggleVisibility } = props;
+
+  const indent = (depth + 1) * 1.5;
+  const termStyle = {
+    color: {
+      "!": "red",
+      "-": "gray",
+      "(-)": "gray",
+    }[status],
+  };
+
+  return (
+    <div style={{ paddingLeft: indent + "em" }}>
+      {DESCENDANTS_MAP[code].length ? (
+        <span
+          onClick={toggleVisibility.bind(null, code)}
+          style={{ cursor: "pointer" }}
+        >
+          {isExpanded ? "⊟" : "⊞"}
+        </span>
+      ) : null}
+      <span style={termStyle}>{term}</span>{" "}
+      <span>
+        (<code>{code}</code>)
+      </span>
+    </div>
   );
 }
 

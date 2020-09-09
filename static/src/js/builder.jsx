opensafely-core/opencodelists

commit f106ba3986722917dfa573902d8f4fe7d5d7a3d3

Construct a single class declaration
This drops the style attribute in favour of bootstrap classes and
combines all the classes in a single declaration.

static/src/js/builder.jsx
@@ -253,11 +253,12 @@ function Row(props) {
     toggleVisibility,
   } = props;
 
-  const style = { display: isVisible ? "flex" : "none" };
-  const className = row.pipes.length === 0 ? "mt-2" : "mt-0";
+  const visibility = isVisible ? "d-flex" : "d-none";
+  const rowSpacing = row.pipes.length === 0 ? "mt-2" : "mt-0";
+  const className = `row ${rowSpacing} ${visibility}`;
 
   return (
-    <div className="row" style={style} className={className}>
+    <div className={className}>
       <div className="btn-group btn-group-sm" role="group">
         <Button
           code={row.code}

commit 1a8fea18f1dbefc51329be4d0ced19f3f7d36365

Fix rendering of long rows
This was broken in f106ba39.  Original code only worked because the
first `class="row"` is overridden by the second `class="{className}"`.

static/src/js/builder.jsx
@@ -255,7 +255,7 @@ function Row(props) {
 
   const visibility = isVisible ? "d-flex" : "d-none";
   const rowSpacing = row.pipes.length === 0 ? "mt-2" : "mt-0";
-  const className = `row ${rowSpacing} ${visibility}`;
+  const className = `${rowSpacing} ${visibility}`;
 
   return (
     <div className={className}>
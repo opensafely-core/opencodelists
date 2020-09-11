ae8e85b8cdb2186a98b64f83f195968893e8b812
George Hickman <george@ghickman.co.uk>
2020-09-11 09:23:33 +0100

    Extract TermAndCode component into a separate file

--- static/src/js/builder/codelistbuilder.jsx
+++ static/src/js/builder/codelistbuilder.jsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 
+import TermAndCode from "../TermAndCode";
 import { getCookie } from "../utils";
 
 class CodelistBuilder extends React.Component {
@@ -318,55 +319,6 @@ function Button(props) {
     >
       {symbol}
     </button>
-  );
-}
-
-function TermAndCode(props) {
-  const {
-    term,
-    code,
-    pipes,
-    status,
-    hasDescendants,
-    isExpanded,
-    toggleVisibility,
-  } = props;
-
-  const termStyle = {
-    color: {
-      "!": "red",
-      "-": "gray",
-      "(-)": "gray",
-    }[status],
-  };
-
-  return (
-    <div style={{ paddingLeft: "10px", whiteSpace: "nowrap" }}>
-      {pipes.map((pipe, ix) => (
-        <span
-          key={ix}
-          style={{
-            display: "inline-block",
-            textAlign: "center",
-            width: "20px",
-          }}
-        >
-          {pipe}
-        </span>
-      ))}
-      {hasDescendants ? (
-        <span
-          onClick={toggleVisibility.bind(null, code)}
-          style={{ cursor: "pointer" }}
-        >
-          {isExpanded ? "⊟" : "⊞"}
-        </span>
-      ) : null}
-      <span style={termStyle}>{term}</span>{" "}
-      <span>
-        (<code>{code}</code>)
-      </span>
-    </div>
   );
 }
 

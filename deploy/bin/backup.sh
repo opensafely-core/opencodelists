bd1771828da25bed7523053a25ba0151b9933341
Peter Inglesby <peter.inglesby@gmail.com>
2020-11-02 12:23:32 +0000

    Make shellcheck happy

--- deploy/bin/backup.sh
+++ deploy/bin/backup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-REPO_ROOT=$(dirname $(dirname $(dirname $0)))
+REPO_ROOT=$(dirname "$(dirname "$(dirname "$0")")")
 BACKUP_PATH="/mnt/volume-lon1-01/opencodelists-backups/core-data-$(date +%F).json"
 
-$REPO_ROOT/with_environment.sh \
+"$REPO_ROOT"/with_environment.sh \
         python \
-        $REPO_ROOT/manage.py \
+        "$REPO_ROOT"/manage.py \
         dumpdata \
         builder codelists opencodelists \
         --indent 2 \
         --verbosity 0 \
-        --output $BACKUP_PATH
+        --output "$BACKUP_PATH"

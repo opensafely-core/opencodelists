8106f1ef5f8056542585e5a023bd88678e6a7027
Peter Inglesby <peter.inglesby@gmail.com>
2020-11-24 15:43:15 +0000

    Redirect to most recent version

--- codelists/views/codelist.py
+++ codelists/views/codelist.py
@@ -5,5 +5,5 @@ from .decorators import load_codelist
 
 @load_codelist
 def codelist(request, codelist):
-    clv = codelist.versions.order_by("tag").last()
+    clv = codelist.versions.order_by("created_at").last()
     return redirect(clv)

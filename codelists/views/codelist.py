14e856af964eb7f4523d327695bbc338b97db6ac
Peter Inglesby <peter.inglesby@gmail.com>
2021-04-16 10:41:28 +0100

    Don't redirect from codelist to builder
    Previously, the codelist page would redirect to the codelist's latest
    version.  However, we don't want to redirect to a version that's
    currently being edited.

--- codelists/views/codelist.py
+++ codelists/views/codelist.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import redirect
 
 from .decorators import load_codelist
@@ -5,5 +6,11 @@ from .decorators import load_codelist
 
 @load_codelist
 def codelist(request, codelist):
-    clv = codelist.versions.order_by("created_at").last()
-    return redirect(clv)
+    clv = codelist.latest_version()
+    if clv is not None:
+        return redirect(clv)
+    else:
+        messages.add_message(
+            request, messages.INFO, "This codelist has not been published"
+        )
+        return redirect("/")

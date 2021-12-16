f82f94bf5a99dd2a4d23dfa166904c6a59104357
Peter Inglesby <peter.inglesby@gmail.com>
2021-12-16 16:38:38 +0000

    Update TPP importer for latest data format

--- coding_systems/ctv3/scripts/import_tpp_ctv3_data.py
+++ coding_systems/ctv3/scripts/import_tpp_ctv3_data.py
@@ -7,9 +7,11 @@ from coding_systems.ctv3.models import TPPConcept, TPPRelationship
 
 
 def run(release_dir):
-    def load_records(filename):
-        with open(os.path.join(release_dir, filename + ".csv")) as f:
-            yield from csv.DictReader(f)
+    def load_records(filename, delimiter):
+        with open(
+            os.path.join(release_dir, filename + ".csv"), encoding="latin-1"
+        ) as f:
+            yield from csv.DictReader(f, delimiter=delimiter)
 
     with transaction.atomic():
         TPPRelationship.objects.all().delete()
@@ -17,7 +19,7 @@ def run(release_dir):
 
         TPPConcept.objects.bulk_create(
             TPPConcept(read_code=r["CTV3Code"], description=r["Description"])
-            for r in load_records("ctv3dictionary")
+            for r in load_records("ctv3dictionary", "|")
         )
 
         TPPRelationship.objects.bulk_create(
@@ -26,5 +28,5 @@ def run(release_dir):
                 descendant_id=r["ChildCTV3Code"],
                 distance=r["ChildToParentDistance"],
             )
-            for r in load_records("ctv3hierarchy")
+            for r in load_records("ctv3hierarchy", ",")
         )

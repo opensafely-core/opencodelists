7ab89b229fc2edbb67154c501c8d0a46265dccff
Becky Smith <becky.smith@thedatalab.org>
2022-02-25 16:50:18 +0000

    Update deployment for dokku

--- deploy/bin/backup.sh
+++ deploy/bin/backup.sh
@@ -2,17 +2,16 @@
 
 set -euo pipefail
 
-REPO_ROOT=$(dirname "$(dirname "$(dirname "$0")")")
-BACKUP_DIR="/mnt/volume-lon1-01/opencodelists-backups"
+REPO_ROOT="/app"
+BACKUP_DIR="/storage"
 
-"$REPO_ROOT"/with_environment.sh \
-        python \
-        "$REPO_ROOT"/manage.py \
-        dumpdata \
-        builder codelists opencodelists \
-        --indent 2 \
-        --verbosity 0 \
-        --output "${BACKUP_DIR}/core-data-$(date +%F).json.gz"
+python \
+"$REPO_ROOT"/manage.py \
+dumpdata \
+builder codelists opencodelists \
+--indent 2 \
+--verbosity 0 \
+--output "${BACKUP_DIR}/core-data-$(date +%F).json.gz"
 
 # Keep only the last 30 backups
 find "$BACKUP_DIR" -name "core-data-*.json.gz" | sort | head -n -30 | xargs rm

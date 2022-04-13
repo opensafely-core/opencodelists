9751ff41b5a2012b8e8a366855fc03dd75f9a764
Tom Ward <tomward@fmail.co.uk>
2022-04-13 16:11:11 +0100

    feat: add opentelemetry
    * send spans to honeycomb.io

--- deploy/gunicorn/conf.py
+++ deploy/gunicorn/conf.py
@@ -1,5 +1,11 @@
 from services.logging import logging_config_dict
 
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
 bind = "0.0.0.0:7000"
 
 workers = 8
@@ -12,3 +18,18 @@ errorlog = "-"
 # Configure log structure
 # http://docs.gunicorn.org/en/stable/settings.html#logconfig-dict
 logconfig_dict = logging_config_dict
+
+def post_fork(server, worker):
+    server.log.info("Worker spawned (pid: %s)", worker.pid)
+
+    resource = Resource.create(attributes={"service.name": "opencodelists"})
+
+    trace.set_tracer_provider(TracerProvider(resource=resource))
+    span_processor = BatchSpanProcessor(
+        OTLPSpanExporter(endpoint="https://api.honeycomb.io")
+    )
+    trace.get_tracer_provider().add_span_processor(span_processor)
+
+    from opentelemetry.instrumentation.auto_instrumentation import (  # noqa: F401
+        sitecustomize,
+    )

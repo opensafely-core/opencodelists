50561c28f090e73c98ea6813fc96f033f24a1329
Tom Ward <tomward@fmail.co.uk>
2022-04-14 11:21:56 +0100

    fix: add comments about gunicorn setup

--- deploy/gunicorn/conf.py
+++ deploy/gunicorn/conf.py
@@ -20,6 +20,9 @@ errorlog = "-"
 logconfig_dict = logging_config_dict
 
 
+# Because of Gunicorn's pre-fork web server model, we need to initialise opentelemetry
+# in gunicorn's post_fork method in order to instrument our application process, see:
+# https://opentelemetry-python.readthedocs.io/en/latest/examples/fork-process-model/README.html
 def post_fork(server, worker):
     server.log.info("Worker spawned (pid: %s)", worker.pid)
 
@@ -31,6 +34,8 @@ def post_fork(server, worker):
     )
     trace.get_tracer_provider().add_span_processor(span_processor)
 
+    # initialise the automatic Django instrumentation
+    # https://opentelemetry-python-contrib.readthedocs.io/en/latest/instrumentation/django/django.html#module-opentelemetry.instrumentation.django
     from opentelemetry.instrumentation.auto_instrumentation import (  # noqa: F401
         sitecustomize,
     )

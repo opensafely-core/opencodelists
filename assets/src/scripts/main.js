5c97cab7f8d283ea6cf69f7543bfe43b279d7afd
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2022-05-24 12:23:25 +0100

    Replace webpack bundling with Vite

---
+++ assets/src/scripts/main.js
@@ -0,0 +1,1 @@
+console.log("Silence is golden");
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,27 @@
+import legacy from "@vitejs/plugin-legacy";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/static/bundle/",
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: {
+        builder: "/assets/src/scripts/builder/index.jsx",
+        codelist: "/assets/src/scripts/codelist.js",
+        main: "/assets/src/scripts/main.js",
+        tree: "/assets/src/scripts/tree/index.jsx",
+      },
+    },
+    outDir: "assets/dist/bundle",
+    emptyOutDir: true,
+  },
+  clearScreen: false,
+  plugins: [
+    legacy({
+      targets: ["last 2 versions, not dead, > 2%"],
+    }),
+  ],
   test: {
     globals: true,
     environment: "jsdom",

2657619d5650ed18f445a05c337f904aa961888c
Becky Smith <becky.smith@thedatalab.org>
2022-09-20 13:12:15 +0100

    Update deprecated settings and methods

--- services/logging.py
+++ services/logging.py
@@ -23,6 +23,7 @@ def timestamper(logger, log_method, event_dict):
 
 
 pre_chain = [
+    structlog.contextvars.merge_contextvars,
     # Add the log level and a timestamp to the event_dict if the log entry
     # is not from structlog.
     structlog.stdlib.add_log_level,
@@ -32,6 +33,7 @@ pre_chain = [
 
 structlog.configure(
     processors=[
+        structlog.contextvars.merge_contextvars,
         structlog.stdlib.filter_by_level,
         timestamper,
         structlog.stdlib.add_logger_name,
@@ -42,7 +44,6 @@ structlog.configure(
         structlog.processors.UnicodeDecoder(),
         structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
     ],
-    context_class=structlog.threadlocal.wrap_dict(dict),
     logger_factory=structlog.stdlib.LoggerFactory(),
     wrapper_class=structlog.stdlib.BoundLogger,
     cache_logger_on_first_use=True,

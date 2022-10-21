da0405f1df0da6baac82de31b911c26f0f49aa1c
Becky Smith <becky.smith@thedatalab.org>
2022-10-21 10:38:15 +0100

    Refactor ctv3 coding_system to use CodingSystem class

--- coding_systems/ctv3/coding_system.py
+++ coding_systems/ctv3/coding_system.py
@@ -4,147 +4,147 @@ from django.db.models import Q
 
 from opencodelists.db_utils import query
 
+from ..base.coding_system_base import BaseCodingSystem
 from .models import RawConceptTermMapping, TPPConcept, TPPRelationship
 
-name = "CTV3 (Read V3)"
-short_name = "CTV3"
 
-root = "....."
+class CodingSystem(BaseCodingSystem):
+    id = "ctv3"
+    name = "CTV3 (Read V3)"
+    short_name = "CTV3"
+    root = "....."
 
-
-def lookup_names(codes):
-    return dict(
-        TPPConcept.objects.filter(read_code__in=codes).values_list(
-            "read_code", "description"
+    def lookup_names(self, codes):
+        return dict(
+            TPPConcept.objects.using(self.db)
+            .filter(read_code__in=codes)
+            .values_list("read_code", "description")
         )
-    )
 
-
-def search_by_term(term):
-    tpp_read_codes = set(
-        TPPConcept.objects.filter(description__contains=term)
-        .values_list("read_code", flat=True)
-        .distinct()
-    )
-    raw_read_codes = set(
-        RawConceptTermMapping.objects.filter(
-            Q(term__name_1__contains=term)
-            | Q(term__name_2__contains=term)
-            | Q(term__name_3__contains=term)
+    def search_by_term(self, term):
+        tpp_read_codes = set(
+            TPPConcept.objects.using(self.db)
+            .filter(description__contains=term)
+            .values_list("read_code", flat=True)
+            .distinct()
         )
-        .values_list("concept_id", flat=True)
-        .distinct()
-    )
-    return tpp_read_codes | raw_read_codes
+        raw_read_codes = set(
+            RawConceptTermMapping.using(self.db)
+            .objects.filter(
+                Q(term__name_1__contains=term)
+                | Q(term__name_2__contains=term)
+                | Q(term__name_3__contains=term)
+            )
+            .values_list("concept_id", flat=True)
+            .distinct()
+        )
+        return tpp_read_codes | raw_read_codes
 
+    def search_by_code(self, code):
+        tpp_read_codes = set(
+            TPPConcept.objects.using(self.db)
+            .filter(read_code=code)
+            .values_list("read_code", flat=True)
+            .distinct()
+        )
+        raw_read_codes = set(
+            RawConceptTermMapping.using(self.db)
+            .objects.filter(concept_id=code)
+            .values_list("concept_id", flat=True)
+            .distinct()
+        )
+        return tpp_read_codes | raw_read_codes
 
-def search_by_code(code):
-    tpp_read_codes = set(
-        TPPConcept.objects.filter(read_code=code)
-        .values_list("read_code", flat=True)
-        .distinct()
-    )
-    raw_read_codes = set(
-        RawConceptTermMapping.objects.filter(concept_id=code)
-        .values_list("concept_id", flat=True)
-        .distinct()
-    )
-    return tpp_read_codes | raw_read_codes
+    def ancestor_relationships(self, codes):
+        codes = list(codes)
+        relationship_table = TPPRelationship._meta.db_table
+        placeholders = ", ".join(["%s"] * len(codes))
+        sql = f"""
+        WITH RECURSIVE tree(ancestor_id, descendant_id) AS (
+        SELECT ancestor_id, descendant_id
+        FROM {relationship_table}
+        WHERE descendant_id IN ({placeholders}) AND distance = 1
 
+        UNION
 
-def ancestor_relationships(codes):
-    codes = list(codes)
-    relationship_table = TPPRelationship._meta.db_table
-    placeholders = ", ".join(["%s"] * len(codes))
-    sql = f"""
-    WITH RECURSIVE tree(ancestor_id, descendant_id) AS (
-      SELECT ancestor_id, descendant_id
-      FROM {relationship_table}
-      WHERE descendant_id IN ({placeholders}) AND distance = 1
+        SELECT r.ancestor_id, r.descendant_id
+        FROM {relationship_table} r
+        INNER JOIN tree t
+            ON r.descendant_id = t.ancestor_id
+        WHERE distance = 1
+        )
 
-      UNION
+        SELECT ancestor_id, descendant_id FROM tree
+        """
 
-      SELECT r.ancestor_id, r.descendant_id
-      FROM {relationship_table} r
-      INNER JOIN tree t
-        ON r.descendant_id = t.ancestor_id
-      WHERE distance = 1
-    )
+        return query(sql, codes, database=self.db)
 
-    SELECT ancestor_id, descendant_id FROM tree
-    """
+    def descendant_relationships(self, codes):
+        codes = list(codes)
+        relationship_table = TPPRelationship._meta.db_table
+        placeholders = ", ".join(["%s"] * len(codes))
+        sql = f"""
+        WITH RECURSIVE tree(ancestor_id, descendant_id) AS (
+        SELECT ancestor_id, descendant_id
+        FROM {relationship_table}
+        WHERE ancestor_id IN ({placeholders}) AND distance = 1
 
-    return query(sql, codes)
+        UNION
 
+        SELECT r.ancestor_id, r.descendant_id
+        FROM {relationship_table} r
+        INNER JOIN tree t
+            ON r.ancestor_id = t.descendant_id
+        WHERE distance = 1
+        )
 
-def descendant_relationships(codes):
-    codes = list(codes)
-    relationship_table = TPPRelationship._meta.db_table
-    placeholders = ", ".join(["%s"] * len(codes))
-    sql = f"""
-    WITH RECURSIVE tree(ancestor_id, descendant_id) AS (
-      SELECT ancestor_id, descendant_id
-      FROM {relationship_table}
-      WHERE ancestor_id IN ({placeholders}) AND distance = 1
+        SELECT ancestor_id, descendant_id FROM tree
+        """
 
-      UNION
+        return query(sql, codes, database=self.db)
 
-      SELECT r.ancestor_id, r.descendant_id
-      FROM {relationship_table} r
-      INNER JOIN tree t
-        ON r.ancestor_id = t.descendant_id
-      WHERE distance = 1
-    )
+    def code_to_term(self, codes):
+        lookup = self.lookup_names(codes)
+        unknown = set(codes) - set(lookup)
+        return {**lookup, **{code: "Unknown" for code in unknown}}
 
-    SELECT ancestor_id, descendant_id FROM tree
-    """
+    def codes_by_type(self, codes, hierarchy):
+        """
+        Group codes by their Concept "types"
 
-    return query(sql, codes)
+        We treat the children of CTV3's root Concept as types.  However, CTV3
+        Concepts can be descended from more than one of these "types", so each
+        grouping of codes can have an overlap with other groupings.
+        """
 
+        if not codes:
+            # The hierarchy will be empty, and so looking up the children of the
+            # root will fail.
+            return {}
 
-def code_to_term(codes):
-    lookup = lookup_names(codes)
-    unknown = set(codes) - set(lookup)
-    return {**lookup, **{code: "Unknown" for code in unknown}}
+        # Treat children of CTV3 root as types.  This will include any unknown codes.
+        types = hierarchy.child_map[self.root]
+        concepts = TPPConcept.objects.using(self.db).filter(read_code__in=types)
+        terms_by_type = {c.read_code: c.description for c in concepts}
 
+        lookup = collections.defaultdict(list)
 
-def codes_by_type(codes, hierarchy):
-    """
-    Group codes by their Concept "types"
+        for code in codes:
+            if code in types:
+                # This code is one of the top-level types, and so will not be captured
+                # below.
+                lookup[terms_by_type.get(code, "[Unknown]")].append(code)
+                continue
 
-    We treat the children of CTV3's root Concept as types.  However, CTV3
-    Concepts can be descended from more than one of these "types", so each
-    grouping of codes can have an overlap with other groupings.
-    """
+            # get groups for this
+            groups = hierarchy.ancestors(code) & types
 
-    if not codes:
-        # The hierarchy will be empty, and so looking up the children of the
-        # root will fail.
-        return {}
+            # Remove Concept "Additional values" when there is at least one other
+            # type to use.
+            if len(groups) > 1 and "X78tJ" in groups:
+                groups.remove("X78tJ")
 
-    # Treat children of CTV3 root as types.  This will include any unknown codes.
-    types = hierarchy.child_map[root]
-    concepts = TPPConcept.objects.filter(read_code__in=types)
-    terms_by_type = {c.read_code: c.description for c in concepts}
-
-    lookup = collections.defaultdict(list)
-
-    for code in codes:
-        if code in types:
-            # This code is one of the top-level types, and so will not be captured
-            # below.
-            lookup[terms_by_type.get(code, "[Unknown]")].append(code)
-            continue
-
-        # get groups for this
-        groups = hierarchy.ancestors(code) & types
-
-        # Remove Concept "Additional values" when there is at least one other
-        # type to use.
-        if len(groups) > 1 and "X78tJ" in groups:
-            groups.remove("X78tJ")
-
-        for group_code in groups:
-            group_term = terms_by_type[group_code]
-            lookup[group_term].append(code)
-    return dict(lookup)
+            for group_code in groups:
+                group_term = terms_by_type[group_code]
+                lookup[group_term].append(code)
+        return dict(lookup)
--- coding_systems/ctv3/tests/test_coding_system.py
+++ coding_systems/ctv3/tests/test_coding_system.py
@@ -1,34 +1,40 @@
-from coding_systems.ctv3.coding_system import (
-    ancestor_relationships,
-    code_to_term,
-    descendant_relationships,
-    lookup_names,
-)
+import pytest
+
+from coding_systems.ctv3.coding_system import CodingSystem
 from coding_systems.ctv3.models import TPPConcept, TPPRelationship
 
 
-def test_lookup_names():
-    for code in [".....", "11111", "22222"]:
-        TPPConcept.objects.create(read_code=code, description=f"Concept {code}")
+@pytest.fixture
+def coding_system():
+    yield CodingSystem(database_alias="ctv3_test_20200101")
 
-    assert lookup_names(["11111", "22222", "99999"]) == {
+
+def test_lookup_names(coding_system):
+    for code in [".....", "11111", "22222"]:
+        TPPConcept.objects.using("ctv3_test_20200101").create(
+            read_code=code, description=f"Concept {code}"
+        )
+
+    assert coding_system.lookup_names(["11111", "22222", "99999"]) == {
         "11111": "Concept 11111",
         "22222": "Concept 22222",
     }
 
 
-def test_code_to_term():
+def test_code_to_term(coding_system):
     for code in [".....", "11111", "22222"]:
-        TPPConcept.objects.create(read_code=code, description=f"Concept {code}")
+        TPPConcept.objects.using("ctv3_test_20200101").create(
+            read_code=code, description=f"Concept {code}"
+        )
 
-    assert code_to_term(["11111", "22222", "99999"]) == {
+    assert coding_system.code_to_term(["11111", "22222", "99999"]) == {
         "11111": "Concept 11111",
         "22222": "Concept 22222",
         "99999": "Unknown",
     }
 
 
-def test_relationships():
+def test_relationships(coding_system):
     r"""Hierarchy has this structure:
         .
        / \
@@ -51,30 +57,32 @@ def test_relationships():
     ]
 
     for ancestor_code, descendant_code, distance in records:
-        ancestor, _ = TPPConcept.objects.get_or_create(
+        ancestor, _ = TPPConcept.objects.using("ctv3_test_20200101").get_or_create(
             read_code=ancestor_code, defaults={"description": ancestor_code}
         )
-        descendant, _ = TPPConcept.objects.get_or_create(
+        descendant, _ = TPPConcept.objects.using("ctv3_test_20200101").get_or_create(
             read_code=descendant_code, defaults={"description": descendant_code}
         )
-        TPPRelationship.objects.create(
-            ancestor=ancestor, descendant=descendant, distance=distance
+        TPPRelationship.objects.using("ctv3_test_20200101").create(
+            ancestor=ancestor,
+            descendant=descendant,
+            distance=distance,
         )
 
-    assert set(ancestor_relationships(["....."])) == set()
+    assert set(coding_system.ancestor_relationships(["....."])) == set()
 
-    assert set(ancestor_relationships(["11111"])) == {
+    assert set(coding_system.ancestor_relationships(["11111"])) == {
         (".....", "11111"),
     }
 
-    assert set(ancestor_relationships(["33333", "55555"])) == {
+    assert set(coding_system.ancestor_relationships(["33333", "55555"])) == {
         (".....", "11111"),
         (".....", "22222"),
         ("11111", "33333"),
         ("22222", "55555"),
     }
 
-    assert set(descendant_relationships(["....."])) == {
+    assert set(coding_system.descendant_relationships(["....."])) == {
         (".....", "11111"),
         (".....", "22222"),
         ("11111", "33333"),
@@ -83,9 +91,9 @@ def test_relationships():
         ("22222", "55555"),
     }
 
-    assert set(descendant_relationships(["11111"])) == {
+    assert set(coding_system.descendant_relationships(["11111"])) == {
         ("11111", "33333"),
         ("11111", "44444"),
     }
 
-    assert set(descendant_relationships(["33333", "55555"])) == set()
+    assert set(coding_system.descendant_relationships(["33333", "55555"])) == set()

f592f9dadd41a96db33c227ce60be50a6396a384
Becky Smith <becky.smith@thedatalab.org>
2022-10-21 11:07:19 +0100

    Add a CodingSystem class for readv2 This doesn't actually do anything but allows us to support old readv2 codelists

--- coding_systems/readv2/coding_system.py
+++ coding_systems/readv2/coding_system.py
@@ -1,2 +1,7 @@
-name = "Read V2"
-short_name = "Read V2"
+from ..base.coding_system_base import BaseCodingSystem
+
+
+class CodingSystem(BaseCodingSystem):
+    id = "readv2"
+    name = "Read V2"
+    short_name = "Read V2"

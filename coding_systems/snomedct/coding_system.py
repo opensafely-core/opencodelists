126e7fd9f0e01fd21a0d292fdb10912463930278
Becky Smith <becky.smith@thedatalab.org>
2022-10-26 15:54:44 +0100

    Refactor snomedct coding_system to use CodingSystem class

--- coding_systems/snomedct/coding_system.py
+++ coding_systems/snomedct/coding_system.py
@@ -3,142 +3,139 @@ import re
 
 from opencodelists.db_utils import query
 
+from ..base.coding_system_base import BaseCodingSystem
 from .models import FULLY_SPECIFIED_NAME, IS_A, Concept, Description
-
-name = "SNOMED CT"
-short_name = "SNOMED CT"
-
-root = "138875005"
-
 
 term_and_type_pat = re.compile(r"(^.*) \(([\w/ ]+)\)$")
 
 
-def lookup_names(codes):
-    return {
-        description.concept_id: description.term
-        for description in Description.objects.filter(
-            concept__in=codes, type=FULLY_SPECIFIED_NAME, active=True
+class CodingSystem(BaseCodingSystem):
+    id = "snomedct"
+    name = "SNOMED CT"
+    short_name = "SNOMED CT"
+    root = "138875005"
+
+    def lookup_names(self, codes):
+        return {
+            description.concept_id: description.term
+            for description in Description.objects.using(self.db).filter(
+                concept__in=codes, type=FULLY_SPECIFIED_NAME, active=True
+            )
+        }
+
+    def search_by_term(self, term):
+        return set(
+            Concept.objects.using(self.db)
+            .filter(descriptions__term__contains=term, descriptions__active=True)
+            .values_list("id", flat=True)
         )
-    }
 
-
-def search_by_term(term):
-    return set(
-        Concept.objects.filter(
-            descriptions__term__contains=term, descriptions__active=True
-        ).values_list("id", flat=True)
-    )
-
-
-def search_by_code(code):
-    if Concept.objects.filter(id=code).exists():
-        return {code}
-    else:
-        return set()
-
-
-def ancestor_relationships(codes):
-    codes = list(codes)
-    placeholders = ", ".join(["%s"] * len(codes))
-    sql = f"""
-    WITH RECURSIVE tree(parent_id, child_id) AS (
-      SELECT
-        destination_id AS parent_id,
-        source_id AS child_id
-      FROM snomedct_relationship
-      WHERE child_id IN ({placeholders})
-        AND type_id = '{IS_A}'
-        AND active
-
-      UNION
-
-      SELECT
-        r.destination_id AS parent_id,
-        r.source_id AS child_id
-      FROM snomedct_relationship r
-      INNER JOIN tree t
-        ON r.source_id = t.parent_id
-      WHERE r.type_id = '{IS_A}'
-        AND active
-    )
-
-    SELECT parent_id, child_id FROM tree
-    """
-
-    return query(sql, codes)
-
-
-def descendant_relationships(codes):
-    codes = list(codes)
-    placeholders = ", ".join(["%s"] * len(codes))
-    sql = f"""
-    WITH RECURSIVE tree(parent_id, child_id) AS (
-      SELECT
-        destination_id AS parent_id,
-        source_id AS child_id
-      FROM snomedct_relationship
-      WHERE parent_id IN ({placeholders})
-        AND type_id = '{IS_A}'
-        AND active
-
-      UNION
-
-      SELECT
-        r.destination_id AS parent_id,
-        r.source_id AS child_id
-      FROM snomedct_relationship r
-      INNER JOIN tree t
-        ON r.destination_id = t.child_id
-      WHERE r.type_id = '{IS_A}'
-        AND active
-    )
-
-    SELECT parent_id, child_id FROM tree
-    """
-
-    return query(sql, codes)
-
-
-def _iter_code_to_term_and_type(codes: set):
-    for code, term in lookup_names(codes).items():
-        match = term_and_type_pat.match(term)
-        term_and_type = match.groups() if match else (term, "unknown")
-
-        yield code, term_and_type
-
-
-def code_to_term_and_type(codes):
-    codes = set(codes)
-    return dict(_iter_code_to_term_and_type(codes))
-
-
-def code_to_term(codes):
-    lookup = {code: term for code, (term, _) in code_to_term_and_type(codes).items()}
-    unknown = set(codes) - set(lookup)
-    return {**lookup, **{code: "Unknown" for code in unknown}}
-
-
-def codes_by_type(codes, hierarchy):
-    """Group codes by their Type"""
-    # create a lookup of code -> type
-    code_to_type = {
-        code: type.title() for code, (_, type) in code_to_term_and_type(codes).items()
-    }
-
-    code_to_active = {
-        concept.id: concept.active for concept in Concept.objects.filter(id__in=codes)
-    }
-
-    lookup = collections.defaultdict(list)
-
-    for code in codes:
-        type = code_to_type.get(code)
-        if type is None:
-            lookup["[unknown]"].append(code)
-        elif code_to_active[code]:
-            lookup[type].append(code)
+    def search_by_code(self, code):
+        if Concept.objects.using(self.db).filter(id=code).exists():
+            return {code}
         else:
-            lookup[f"[inactive] {type}"].append(code)
+            return set()
 
-    return dict(lookup)
+    def ancestor_relationships(self, codes):
+        codes = list(codes)
+        placeholders = ", ".join(["%s"] * len(codes))
+        sql = f"""
+        WITH RECURSIVE tree(parent_id, child_id) AS (
+          SELECT
+            destination_id AS parent_id,
+            source_id AS child_id
+          FROM snomedct_relationship
+          WHERE child_id IN ({placeholders})
+            AND type_id = '{IS_A}'
+            AND active
+
+          UNION
+
+          SELECT
+            r.destination_id AS parent_id,
+            r.source_id AS child_id
+          FROM snomedct_relationship r
+          INNER JOIN tree t
+            ON r.source_id = t.parent_id
+          WHERE r.type_id = '{IS_A}'
+            AND active
+        )
+
+        SELECT parent_id, child_id FROM tree
+        """
+
+        return query(sql, codes, database=self.db)
+
+    def descendant_relationships(self, codes):
+        codes = list(codes)
+        placeholders = ", ".join(["%s"] * len(codes))
+        sql = f"""
+        WITH RECURSIVE tree(parent_id, child_id) AS (
+          SELECT
+            destination_id AS parent_id,
+            source_id AS child_id
+          FROM snomedct_relationship
+          WHERE parent_id IN ({placeholders})
+            AND type_id = '{IS_A}'
+            AND active
+
+          UNION
+
+          SELECT
+            r.destination_id AS parent_id,
+            r.source_id AS child_id
+          FROM snomedct_relationship r
+          INNER JOIN tree t
+            ON r.destination_id = t.child_id
+          WHERE r.type_id = '{IS_A}'
+            AND active
+        )
+
+        SELECT parent_id, child_id FROM tree
+        """
+
+        return query(sql, codes, database=self.db)
+
+    def _iter_code_to_term_and_type(self, codes: set):
+        for code, term in self.lookup_names(codes).items():
+            match = term_and_type_pat.match(term)
+            term_and_type = match.groups() if match else (term, "unknown")
+
+            yield code, term_and_type
+
+    def code_to_term_and_type(self, codes):
+        codes = set(codes)
+        return dict(self._iter_code_to_term_and_type(codes))
+
+    def code_to_term(self, codes):
+        lookup = {
+            code: term for code, (term, _) in self.code_to_term_and_type(codes).items()
+        }
+        unknown = set(codes) - set(lookup)
+        return {**lookup, **{code: "Unknown" for code in unknown}}
+
+    def codes_by_type(self, codes, hierarchy):
+        """Group codes by their Type"""
+        # create a lookup of code -> type
+        code_to_type = {
+            code: type.title()
+            for code, (_, type) in self.code_to_term_and_type(codes).items()
+        }
+
+        code_to_active = {
+            concept.id: concept.active
+            for concept in Concept.objects.using(self.db).filter(id__in=codes)
+        }
+
+        lookup = collections.defaultdict(list)
+
+        for code in codes:
+            type = code_to_type.get(code)
+            if type is None:
+                lookup["[unknown]"].append(code)
+            elif code_to_active[code]:
+                lookup[type].append(code)
+            else:
+                lookup[f"[inactive] {type}"].append(code)
+
+        return dict(lookup)
--- coding_systems/snomedct/tests/test_coding_system.py
+++ coding_systems/snomedct/tests/test_coding_system.py
@@ -1,29 +1,33 @@
-from coding_systems.snomedct.coding_system import (
-    code_to_term,
-    codes_by_type,
-    lookup_names,
-)
+import pytest
+
+from coding_systems.snomedct.coding_system import CodingSystem
 
 
-def test_codes_by_type(snomedct_data):
-    assert codes_by_type(
-        ["705115006", "239964003", "106077005", "722135009"], None
+@pytest.fixture
+def coding_system():
+    yield CodingSystem(database_alias="snomedct_test_20200101")
+
+
+def test_codes_by_type(snomedct_data, coding_system):
+    assert coding_system.codes_by_type(
+        ["705115006", "239964003", "106077005", "722135009", "99999"], None
     ) == {
         "Core Metadata Concept": ["705115006", "722135009"],
         "Disorder": ["239964003"],
         "Finding": ["106077005"],
+        "[unknown]": ["99999"],
     }
 
 
-def test_lookup_names(snomedct_data):
-    assert lookup_names(["705115006", "239964003", "99999"]) == {
+def test_lookup_names(snomedct_data, coding_system):
+    assert coding_system.lookup_names(["705115006", "239964003", "99999"]) == {
         "239964003": "Soft tissue lesion of elbow region (disorder)",
         "705115006": "Technology Preview module (core metadata concept)",
     }
 
 
-def test_code_to_term(snomedct_data):
-    assert code_to_term(["705115006", "239964003", "99999"]) == {
+def test_code_to_term(snomedct_data, coding_system):
+    assert coding_system.code_to_term(["705115006", "239964003", "99999"]) == {
         "239964003": "Soft tissue lesion of elbow region",
         "705115006": "Technology Preview module",
         "99999": "Unknown",

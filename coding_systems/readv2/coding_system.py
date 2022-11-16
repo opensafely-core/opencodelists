3f83e3ebf2e7edfff38c64d1cf1986280e83651f
Becky Smith <becky.smith@thedatalab.org>
2022-11-16 11:28:54 +0000

    Remove readv2 models and convert to DummyCodingSystem Readv2 data has never been imported into OpenCodelists, despite the presence of models and an import_data script

--- coding_systems/readv2/coding_system.py
+++ coding_systems/readv2/coding_system.py
@@ -1,7 +1,7 @@
-from ..base.coding_system_base import BaseCodingSystem
+from ..base.coding_system_base import DummyCodingSystem
 
 
-class CodingSystem(BaseCodingSystem):
+class CodingSystem(DummyCodingSystem):
     id = "readv2"
     name = "Read V2"
     short_name = "Read V2"

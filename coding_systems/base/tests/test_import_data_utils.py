d3c5fa3b4822a3837f2dd5f92c37ce764558ff9a
Becky Smith <becky.smith@thedatalab.org>
2022-12-06 09:19:42 +0000

    Check compatibility when a draft is saved and moved to under review

--- coding_systems/base/tests/test_import_data_utils.py
+++ coding_systems/base/tests/test_import_data_utils.py
@@ -4,6 +4,7 @@ import pytest
 from django.db import DEFAULT_DB_ALIAS, connections
 
 from builder.actions import create_search
+from builder.actions import save as save_draft_for_review
 from codelists.coding_systems import CODING_SYSTEMS
 from codelists.hierarchy import Hierarchy
 from codelists.models import Status
@@ -15,12 +16,15 @@ from coding_systems.versioning.models import CodingSystemRelease, ReleaseState
 
 @pytest.fixture
 def bnf_csr():
-    yield CodingSystemRelease.objects.create(
-        coding_system="bnf",
-        release_name="import-data",
-        valid_from=datetime(2022, 11, 1),
-        state=ReleaseState.READY,
-    )
+    def _csr(valid_from=datetime(2022, 11, 1)):
+        return CodingSystemRelease.objects.create(
+            coding_system="bnf",
+            release_name="import-data",
+            valid_from=valid_from,
+            state=ReleaseState.READY,
+        )
+
+    return _csr
 
 
 @pytest.fixture
@@ -65,17 +69,39 @@ def cleanup_db(csr):
 @pytest.fixture
 def bnf_release(bnf_csr):
     # setup the database as a duplicate of the fixture one
-    setup_db(bnf_csr)
-    yield bnf_csr
-    cleanup_db(bnf_csr)
+    csr = bnf_csr()
+    setup_db(csr)
+    yield csr
+    cleanup_db(csr)
 
 
 @pytest.fixture
 def bnf_release_excl_last_concept(bnf_csr):
     # setup the database as a duplicate of the fixture one, omitting the last concept
-    setup_db(bnf_csr, exclude_last_concept=True)
-    yield bnf_csr
-    cleanup_db(bnf_csr)
+    csr = bnf_csr(datetime(2022, 10, 1))
+    setup_db(csr, exclude_last_concept=True)
+    yield csr
+    cleanup_db(csr)
+
+
+@pytest.fixture
+def bnf_releases(bnf_csr):
+    # setup multiple databases as duplicates of the fixture one, with different dates
+
+    # earlier than the release the codelist versions are created with
+    csr_20190101 = bnf_csr(datetime(2019, 1, 1))
+    setup_db(csr_20190101)
+    # earlier than bnf_release_excl_last_concept
+    csr_20220901 = bnf_csr(datetime(2022, 9, 1))
+    setup_db(csr_20220901)
+    # later than bnf_release_excl_last_concept
+    csr_20221201 = bnf_csr(datetime(2022, 12, 1))
+    setup_db(csr_20221201)
+
+    yield
+
+    for csr in [csr_20190101, csr_20220901, csr_20221201]:
+        cleanup_db(csr)
 
 
 def test_update_codelist_version_compatibility_no_searches(
@@ -153,3 +179,46 @@ def test_update_codelist_version_compatibility_with_search_but_mismatched_hierar
     # this version has a search that returns identical results in the new release
     # but the hierarchies differ, so it is not compatible
     assert not bnf_review_version_with_search.compatible_releases.exists()
+
+
+def test_save_codelist_draft_updates_compatibility(
+    bnf_version_with_search, coding_systems_tmp_path, bnf_release
+):
+    assert bnf_version_with_search.status == Status.DRAFT
+    assert not bnf_version_with_search.compatible_releases.exists()
+    save_draft_for_review(draft=bnf_version_with_search)
+
+    assert bnf_version_with_search.compatible_releases.exists()
+
+
+def test_save_codelist_draft_updates_compatibility_multiple_releases(
+    bnf_version_with_search,
+    coding_systems_tmp_path,
+    bnf_releases,
+    bnf_release_excl_last_concept,
+):
+    # In this test, we have a draft created with release `bnf_test_20200101`
+    # The `bnf_releases` fixture gives us 3 other releases, which are duplicates
+    # of the data in `bnf_test_20200101`, so would all be compatible
+    # These have dates 20190901, 20221001, 20221201
+    # bnf_release_excl_last_concept is not compatible, and has date 20221101
+
+    # When the draft version is saved for review, releases with more recent valid_from
+    # dates are checked for compatibility in order from oldest to newest. If a release
+    # is found to be incompatible, no later releases are checked.
+    #
+    # i.e. for this draft, releases are checked in this order:
+    # (bnf_import-data_20190901 is skipped because it's earlier than the draft's cs release)
+    # 1) bnf_import-data_20220901: compatible
+    # 2) bnf_import-data_20221101: incompatible
+    # 3) bnf_import-data_20221201: compatible (but later than an incompatible release, so not checked)
+
+    assert bnf_version_with_search.status == Status.DRAFT
+    assert not bnf_version_with_search.compatible_releases.exists()
+    save_draft_for_review(draft=bnf_version_with_search)
+
+    assert bnf_version_with_search.compatible_releases.count() == 1
+    assert (
+        bnf_version_with_search.compatible_releases.first().database_alias
+        == "bnf_import-data_20220901"
+    )

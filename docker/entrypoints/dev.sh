f9c110e74415904e00a22d9fe67430051efb64b9
Becky Smith <becky.smith@thedatalab.org>
2023-12-18 13:39:58 +0000

    chown staticfiles to non-root user after running collectstatic

--- docker/entrypoints/dev.sh
+++ docker/entrypoints/dev.sh
@@ -5,4 +5,6 @@ set -euo pipefail
 ./manage.py migrate
 ./manage.py collectstatic --no-input --clear | grep -v '^Deleting '
 
+chown -R 1000:1000 /app/staticfiles
+
 exec "$@"
--- docker/entrypoints/prod.sh
+++ docker/entrypoints/prod.sh
@@ -5,4 +5,6 @@ set -euo pipefail
 ./manage.py migrate
 ./manage.py collectstatic --no-input
 
+chown -R 10003:10003 /app/staticfiles
+
 exec gunicorn opencodelists.wsgi --config=deploy/gunicorn/conf.py

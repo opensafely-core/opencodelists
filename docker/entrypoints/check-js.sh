25dba21a5cfb6bef853b8268d744fb6350df4dc9
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2024-02-05 10:53:06 +0000

    Update Dockerfile with new assets location

--- docker/entrypoints/check-js.sh
+++ docker/entrypoints/check-js.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 npx prettier . --check
 
 echo Running eslint
-npx eslint static/src/js/builder/* static/src/js/hierarchy.js
+npx eslint assets/src/scripts/builder/* assets/src/scripts/hierarchy.js

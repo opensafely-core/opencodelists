b43fc7b991403156bdad26bd3cac9bac478bc8e3
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2024-02-06 15:35:35 +0000

    Replace Google Fonts with local font files

--- assets/src/scripts/base.js
+++ assets/src/scripts/base.js
@@ -1,3 +1,10 @@
+// Fonts
+import "@fontsource/public-sans/400.css";
+import "@fontsource/public-sans/500.css";
+import "@fontsource/public-sans/600.css";
+import "@fontsource/public-sans/700.css";
+
+// Styles
 import "../styles/base.css";
 
 if (document.location.hostname === "www.opencodelists.org") {

8985612600eb2ca810057fb0978114dda29d7662
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2024-02-06 13:39:59 +0000

    Add vite JS deps and config

---
+++ vite.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig({
+  base: "/static/",
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: {
+        base: "assets/src/scripts/base.js",
+        builder: "assets/src/scripts/builder/index.jsx",
+        codelist: "assets/src/scripts/codelist.js",
+        tree: "assets/src/scripts/tree/index.jsx",
+      },
+    },
+    outDir: "assets/dist",
+    emptyOutDir: true,
+  },
+  server: {
+    origin: "http://localhost:5173",
+  },
+  clearScreen: false,
+  plugins: [react()],
+});

005ec2ef7c1501c28d0e0eec75efe0d67410c4f9
Steven Maude <git@stevenmaude.co.uk>
2024-11-28 19:21:57 +0000

    Rename settings to use clearer, more modern terms

--- opencodelists/templatetags/markdown_filter.py
+++ opencodelists/templatetags/markdown_filter.py
@@ -12,8 +12,8 @@ def markdown_filter(text):
     text = markdown2.markdown(text)
     html = bleach.clean(
         text,
-        tags=settings.MARKDOWN_FILTER_WHITELIST_TAGS,
-        attributes=settings.MARKDOWN_FILTER_WHITELIST_ATTRIBUTES,
-        css_sanitizer=settings.MARKDOWN_FILTER_WHITELIST_STYLES,
+        tags=settings.MARKDOWN_FILTER_ALLOWLIST_TAGS,
+        attributes=settings.MARKDOWN_FILTER_ALLOWLIST_ATTRIBUTES,
+        css_sanitizer=settings.MARKDOWN_FILTER_ALLOWLIST_STYLES,
     )
     return html

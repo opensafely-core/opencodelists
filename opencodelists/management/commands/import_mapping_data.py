65486dec90f25554d9c1737e549f58b7c30172eb
Mike Kelly <mike.kelly@thedatalab.org>
2024-12-02 11:51:08 +0000

    Find mappings by import not file system inspection.
    This is simpler and means this code doesn't need to worry about the filesystem,
    platform independence etc.
    
    As a bonus the command line entry can now just be the mapping name, and doesn't
    need to include "mappings.". Also did updates in tests/README where this is
    relevant.

--- opencodelists/management/commands/import_mapping_data.py
+++ opencodelists/management/commands/import_mapping_data.py
@@ -1,23 +1,16 @@
-import os
-from importlib import import_module
-from pathlib import Path
+import importlib
 
 from django.core.management import BaseCommand
 
-
-def possible_modules():
-    return [
-        str(path.parent).replace(os.sep, ".")
-        for path in Path("mappings").rglob("import_data.py")
-    ]
+from mappings import mappings
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument("dataset", choices=possible_modules())
+        parser.add_argument("dataset", choices=mappings.keys())
         parser.add_argument("release_dir")
 
     def handle(self, dataset, release_dir, **kwargs):
-        mod = import_module(dataset + ".import_data")
+        mod = importlib.import_module(f"mappings.{dataset}.import_data")
         fn = getattr(mod, "import_data")
         fn(release_dir)

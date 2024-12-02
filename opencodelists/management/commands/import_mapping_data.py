ddf95c658bef4efa7d1b7c1a7c073069eeb345ca
Mike Kelly <mike.kelly@thedatalab.org>
2024-12-02 12:03:58 +0000

    Add parser description/help and better variable names.
    Note that `release_dir` was misleading in particular as it's not always a dir,
    depending on the mapping. Maybe this was once accurate but got out of date.

--- opencodelists/management/commands/import_mapping_data.py
+++ opencodelists/management/commands/import_mapping_data.py
@@ -1,4 +1,4 @@
-import importlib
+from importlib import import_module
 
 from django.core.management import BaseCommand
 
@@ -7,10 +7,17 @@ from mappings import mappings
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument("dataset", choices=mappings.keys())
-        parser.add_argument("release_dir")
+        parser.description = "Import a coding system mapping"
+        parser.add_argument("mapping", choices=mappings.keys(), help="Mapping name")
+        parser.add_argument(
+            "source",
+            help=(
+                "File or dir that defines the mapping, see import_data.py in "
+                "the mapping's package to understand how it is used"
+            ),
+        )
 
-    def handle(self, dataset, release_dir, **kwargs):
-        mod = importlib.import_module(f"mappings.{dataset}.import_data")
-        fn = getattr(mod, "import_data")
-        fn(release_dir)
+    def handle(self, mapping, source, **kwargs):
+        mapping_import_data_module = import_module(f"mappings.{mapping}.import_data")
+        import_data = getattr(mapping_import_data_module , "import_data")
+        import_data(source)

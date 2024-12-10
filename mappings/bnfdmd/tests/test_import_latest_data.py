a499640825d6956d0f7946610187b73fdd246136
Jon Massey <jon.massey@thedatalab.org>
2024-12-10 00:57:33 +0000

    Add import_release() for bnfdmd mapping
    The "import latest data" management command that
    is executed on a scheduled basis expects an
    import_release() function. This is implemented
    here and unzips the (downloaded, or manually
    copied) zip file and calls import_data on the
    resultant xlsx file.
    
    Also provided are tests which mock the responses
    of the NHSBSA website and ensure that the releases
    are correctly parsed and the latest selected.
    
    These mirror the "import latest data" tests for
    coding systems.

---
+++ mappings/bnfdmd/tests/test_import_latest_data.py
@@ -0,0 +1,22 @@
+import datetime
+from pathlib import Path
+from unittest.mock import patch
+
+from django.core.management import call_command
+
+
+@patch("mappings.bnfdmd.import_data.import_release")
+def test_calls_import_release_function_with_extracted_release_metadata(
+    mock_import_release, tmpdir, mock_data_download
+):
+    call_command(
+        "import_latest_data",
+        "mappings.bnfdmd",
+        tmpdir,
+    )
+    mock_import_release.assert_called_once()
+    mock_import_release.assert_called_with(
+        Path(tmpdir) / "BNF Snomed Mapping data 20240101.zip",
+        "20240101",
+        datetime.datetime(2024, 1, 1, 0, 0),
+    )

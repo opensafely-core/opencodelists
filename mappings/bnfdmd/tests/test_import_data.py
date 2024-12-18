opensafely-core/opencodelists

commit 78b2ada13f9c7da78d3526297b22b3673f7b4358

Add import_release() for bnfdmd mapping
The "import latest data" management command that
is executed on a scheduled basis expects an
import_release() function. This is implemented
here and unzips the (downloaded, or manually
copied) zip file and calls import_data on the
resultant xlsx file.

Also provided are tests which mock the responses
of the NHSBSA website and ensure that the releases
are correctly parsed and the latest selected.

These mirror the "import latest data" tests for
coding systems.

mappings/bnfdmd/import_data.py
@@ -1,7 +1,15 @@
+import os
+from tempfile import TemporaryDirectory
+from zipfile import ZipFile
+
+import structlog
 from django.db import transaction
 from openpyxl import load_workbook
 
 from .models import Mapping
+
+
+logger = structlog.get_logger()
 
 
 def import_data(filename):
@@ -35,3 +43,11 @@ def import_data(filename):
             Mapping(dmd_code=r[0], dmd_type=r[1], bnf_concept_id=r[2])
             for r in load_records()
         )
+
+
+def import_release(release_zipfile):
+    with TemporaryDirectory() as tempdir:
+        release_zip = ZipFile(str(release_zipfile))
+        logger.info("Extracting", release_zip=release_zip.filename)
+        release_zip.extractall(path=tempdir)
+        import_data(os.path.join(tempdir, release_zipfile.replace(".zip", ".xlsx")))

mappings/bnfdmd/tests/conftest.py
@@ -1,0 +1,33 @@
+from unittest.mock import patch
+
+import pytest
+import responses
+
+
+def add_response(rsps, filename, release_date):
+    rsps.add(
+        responses.GET,
+        "https://www.nhsbsa.nhs.uk/prescription-data/understanding-our-data/bnf-snomed-mapping",
+        body=f"""
+            <p><a href="/sites/default/files/{release_date}/{filename.replace(' ','%20')}">January 2024 (ZIP file: 16.76MB)</a></p>
+            <p><a href="/sites/default/files/2023-01/BNF%20Snomed%20Mapping%20data%2020230116.zip">January 2023 (ZIP file: 16.76MB)</a></p>
+        """,
+        status=200,
+    )
+
+
+@pytest.fixture
+def mocked_responses():
+    with responses.RequestsMock() as rsps:
+        yield rsps
+
+
+@pytest.fixture
+def mock_data_download(mocked_responses):
+    filename = "BNF%20Snomed%20Mapping%20data%2020240101.zip"
+    add_response(mocked_responses, filename, "2024-01")
+    with patch(
+        "mappings.bnfdmd.data_downloader.Downloader.get_file",
+        return_value=filename,
+    ):
+        yield

mappings/bnfdmd/tests/test_import_latest_data.py
@@ -1,0 +1,22 @@
+import datetime
+from pathlib import Path
+from unittest.mock import patch
+
+from django.core.management import call_command
+
+
+@patch("mappings.bnfdmd.import_data.import_release")
+def test_calls_import_release_function_with_extracted_release_metadata(
+    mock_import_release, tmpdir, mock_data_download
+):
+    call_command(
+        "import_latest_data",
+        "mappings.bnfdmd",
+        tmpdir,
+    )
+    mock_import_release.assert_called_once()
+    mock_import_release.assert_called_with(
+        Path(tmpdir) / "BNF Snomed Mapping data 20240101.zip",
+        "20240101",
+        datetime.datetime(2024, 1, 1, 0, 0),
+    )

commit 18debcfb33ab2b10440b0479ee13330d3335dc7b

BNF to dm+d downloader hotfixes (#2254)
* Create mapping download dir if needed

* Match function signature to import script call

* Unit test bnf-dmd import_release

+ minimal testing release zipfile fixture
+ fix path handling bug

* Improve testing of import_latest_data for bnfdmd

Clarify name.

Use autospec to ensure mocked function matches
signature of real function. Although this is
increasing coupling between this test and the
import_release function, this test is testing that
this function is called correctly with the right
args and part of that correctness is that the
correct number of arguments are called.

This autospec addition would have caught the bug
introduced in 78b2ada by the omission of the
release_name, valid_from args from
bnfdmd.import_data.import_release and guards their
future removal.

mappings/bnfdmd/data_downloader.py
@@ -111,6 +111,8 @@ class Downloader:
 
     def get_file(self, url, filepath):
         logger.info("Starting download", download_filepath=filepath)
+        if not filepath.parent.exists():
+            filepath.parent.mkdir(parents=True)
         with requests.get(url, stream=True) as resp:
             resp.raise_for_status()
             with open(filepath, "wb") as f:

mappings/bnfdmd/import_data.py
@@ -45,9 +45,11 @@ def import_data(filename):
         )
 
 
-def import_release(release_zipfile):
+def import_release(release_zipfile, release_name, valid_from):
     with TemporaryDirectory() as tempdir:
         release_zip = ZipFile(str(release_zipfile))
         logger.info("Extracting", release_zip=release_zip.filename)
         release_zip.extractall(path=tempdir)
-        import_data(os.path.join(tempdir, release_zipfile.replace(".zip", ".xlsx")))
+        import_data(
+            os.path.join(tempdir, release_zipfile.name.replace(".zip", ".xlsx"))
+        )

mappings/bnfdmd/tests/conftest.py
@@ -1,7 +1,16 @@
+from pathlib import Path
 from unittest.mock import patch
 
 import pytest
 import responses
+
+
+MOCK_BNFDMD_IMPORT_DATA_PATH = (
+    Path(__file__).parents[1]
+    / "fixtures"
+    / "import_resources"
+    / "BNF Snomed Mapping data 20241119.zip"
+)
 
 
 def add_response(rsps, filename, release_date):

mappings/bnfdmd/tests/test_import_data.py
@@ -1,0 +1,37 @@
+from mappings.bnfdmd.import_data import import_release
+from mappings.bnfdmd.models import Mapping
+
+from .conftest import MOCK_BNFDMD_IMPORT_DATA_PATH
+
+
+def test_import_release():
+    """
+    import mock data
+    This consists of a minimal mapping file covering
+    AMP/AMPP/VMP/VMPPs both with and without BNF mapping entries:
+
+    |VMP / VMPP/ AMP / AMPP|BNF Code        |BNF Name                |SNOMED Code       |
+    |----------------------|----------------|------------------------|------------------|
+    |VMP                   | 0206020T0AAAGAG| Verapamil 160mg tablets| 42217211000001101|
+    |VMPP                  | 0206020T0AAAGAG| Verapamil 160mg tablets| 982511000001103  |
+    |AMP                   | 0212000Y0BBAAAA| Zocor 10mg tablets     | 108111000001106  |
+    |AMPP                  | 0212000Y0BBAAAA| Zocor 10mg tablets     | 1328211000001104 |
+    |AMP                   |                |                        | 5409611000001107 |
+    |AMPP                  |                |                        | 5409811000001106 |
+    |VMP                   |                |                        | 3377411000001106 |
+    |VMPP                  |                |                        | 1139011000001107 |
+
+    """
+
+    import_release(MOCK_BNFDMD_IMPORT_DATA_PATH, None, None)
+
+    assert Mapping.objects.count() == 4
+    for dmd_type, bnf, dmd in [
+        ("AMP", "0212000Y0BBAAAA", "108111000001106"),
+        ("AMPP", "0212000Y0BBAAAA", "1328211000001104"),
+        ("VMP", "0206020T0AAAGAG", "42217211000001101"),
+        ("VMPP", "0206020T0AAAGAG", "982511000001103"),
+    ]:
+        mapping = Mapping.objects.get(dmd_type=dmd_type)
+        assert mapping.dmd_code == dmd
+        assert mapping.bnf_concept_id == bnf

mappings/bnfdmd/tests/test_import_latest_data.py
@@ -5,10 +5,8 @@ from unittest.mock import patch
 from django.core.management import call_command
 
 
-@patch("mappings.bnfdmd.import_data.import_release")
-def test_calls_import_release_function_with_extracted_release_metadata(
-    mock_import_release, tmpdir, mock_data_download
-):
+@patch("mappings.bnfdmd.import_data.import_release", autospec=True)
+def test_calls_import_release_function(mock_import_release, tmpdir, mock_data_download):
     call_command(
         "import_latest_data",
         "mappings.bnfdmd",
6b46f90aac715803bf207f3ff89f5e22fa18d7d7
Thomas O'Dwyer <thomas.odwyer@thedatalab.org>
2025-01-08 12:21:11 +0000

    Move searches list group in to Search component

--- assets/src/scripts/components/Search.jsx
+++ assets/src/scripts/components/Search.jsx
@@ -3,43 +3,64 @@ import React from "react";
 import { Button, Form, ListGroup } from "react-bootstrap";
 import { getCookie } from "../_utils";
 
-function Search({ search }) {
-  return search.delete_url ? (
-    <ListGroup.Item
-      action
-      active={search.active}
-      className="py-1 px-2"
-      href={search.url}
-    >
-      <Form action={search.delete_url} method="post">
-        <Form.Control
-          name="csrfmiddlewaretoken"
-          type="hidden"
-          value={getCookie("csrftoken")}
-        />
-        {search.term_or_code}
-        <Button
-          aria-label="remove search"
-          className="float-right p-0 px-1"
-          name="delete-search"
-          type="submit"
-          size="sm"
-          variant="danger"
-        >
-          &times;
-        </Button>
-      </Form>
-    </ListGroup.Item>
-  ) : (
-    <ListGroup.Item
-      action
-      active={search.active}
-      className="py-1 px-2"
-      href={encodeURI(search.url)}
-      key={search.url}
-    >
-      {search.term_or_code}
-    </ListGroup.Item>
+function Search({ draftURL, searches }) {
+  return (
+    <>
+      <h3 className="h6">Searches</h3>
+      <ListGroup>
+        {searches.map((search) => (
+          <React.Fragment key={search.url}>
+            {search.delete_url ? (
+              <ListGroup.Item
+                action
+                active={search.active}
+                className="py-1 px-2"
+                href={search.url}
+              >
+                <Form action={search.delete_url} method="post">
+                  <Form.Control
+                    name="csrfmiddlewaretoken"
+                    type="hidden"
+                    value={getCookie("csrftoken")}
+                  />
+                  {search.term_or_code}
+                  <Button
+                    aria-label="remove search"
+                    className="float-right p-0 px-1"
+                    name="delete-search"
+                    type="submit"
+                    size="sm"
+                    variant="secondary"
+                  >
+                    &times;
+                  </Button>
+                </Form>
+              </ListGroup.Item>
+            ) : (
+              <ListGroup.Item
+                action
+                active={search.active}
+                className="py-1 px-2"
+                href={encodeURI(search.url)}
+              >
+                {search.term_or_code}
+              </ListGroup.Item>
+            )}
+          </React.Fragment>
+        ))}
+
+        {searches.some((search) => search.active) ? (
+          <ListGroup.Item
+            action
+            className="py-1 px-2 font-italic"
+            href={encodeURI(draftURL)}
+          >
+            show all
+          </ListGroup.Item>
+        ) : null}
+      </ListGroup>
+      <hr />
+    </>
   );
 }
 

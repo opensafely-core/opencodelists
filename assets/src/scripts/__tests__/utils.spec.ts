877f2d075480faa8af432693e7cfacc183a042bd
Thomas O'Dwyer <thomas.odwyer@thedatalab.org>
2025-01-22 11:07:38 +0000

    Add TypeScript tests

---
+++ assets/src/scripts/__tests__/utils.spec.ts
@@ -0,0 +1,20 @@
+import { expect, it } from "vitest";
+import { getCookie, readValueFromPage } from "../_utils";
+
+it("gets the cookie value", () => {
+  const name = "csrftoken";
+  const value = "01JJ6V33G19M1ZFEB4JAQMMTQW";
+  document.cookie = `${name}=${value || ""};`;
+  expect(getCookie(name)).toBe(value);
+});
+
+it("reads the values of a JSON script on the page", () => {
+  const blob = {
+    hello: "world",
+  };
+  document.body.innerHTML = `
+    <script id="all-codes">${JSON.stringify(blob)}</script>
+  `;
+
+  expect(readValueFromPage("all-codes")).toEqual(blob);
+});

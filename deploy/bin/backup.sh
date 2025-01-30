9cc46c0e7683f7dc43e33818e166972182b48e34
Jon Massey <jon.massey@thedatalab.org>
2025-01-30 12:25:22 +0000

    Implement backup sanitisation.
    Removes PII (username, name, email) from User table by replacement with
    fake data from the Faker library.
    
    Username is used as the PK of User and thus appears in FKs across many
    tables, which means these must also be sanitised.
    
    These FKs are configured as "deferrable" which means you are allowed
    to break referential integrity in the course of a transaction.
    
    Should any new tables be added that refer to the User table, these will
    also be updated by this script.
    
    It is important that the tables referring to the user table are
    updated before the user table, otherwise the configured cascade
    behaviour of `DELETE` will be invoked.

--- deploy/bin/backup.sh
+++ deploy/bin/backup.sh
@@ -15,6 +15,7 @@ SENTRY_CRON_URL=$(sentry_cron_url "$SENTRY_DSN" "$SENTRY_MONITOR_NAME")
 BACKUP_DIR="$DATABASE_DIR/backup/db"
 BACKUP_FILENAME="$(date +%F)-db.sqlite3"
 BACKUP_FILEPATH="$BACKUP_DIR/$BACKUP_FILENAME"
+SANITISED_BACKUP_FILEPATH="$BACKUP_DIR/sanitised-$BACKUP_FILENAME"
 
 function run_backup() {
     # Make the backup dir if it doesn't exist.
@@ -23,21 +24,30 @@ function run_backup() {
     # Take a datestamped backup.
     sqlite3 "$DATABASE_DIR/db.sqlite3" ".backup $BACKUP_FILEPATH"
 
-    # Compress the latest backup.
+    # Make a sanitised copy of the backup.
+    cp "$BACKUP_FILEPATH" "$SANITISED_BACKUP_FILEPATH"
+    python "$SCRIPT_DIR/sanitise_backup.py" "$SANITISED_BACKUP_FILEPATH"
+
+    # Compress the latest backups.
     # Zstandard is a fast, modern, lossless data compression algorithm.  It gives
     # marginally better compression ratios than gzip on the backup and much faster
     # compression and particularly decompression.  We want the backup process to be
     # quick as it's a CPU-intensive activity that could affect site performance.
     # --rm flag removes the source file after compression.
     zstd "$BACKUP_FILEPATH" --rm
+    zstd "$SANITISED_BACKUP_FILEPATH" --rm
 
-    # Symlink to the new latest backup to make it easy to discover.
+    # Symlink to the new latest backups to make it easy to discover.
     # Make the target a relative path -- an absolute one won't mean the same thing
     # in the host file system if executed inside a container as we expect.
     ln -sf "$BACKUP_FILENAME.zst" "$BACKUP_DIR/latest-db.sqlite3.zst"
+    ln -sf "$SANITISED_BACKUP_FILEPATH.zst" "$BACKUP_DIR/sanitised-latest-db.sqlite3.zst"
 
-    # Keep only the last 30 days of backups.
+    # Keep only the last 30 days of raw backups.
     find "$BACKUP_DIR" -name "*-db.sqlite3.zst" -type f -mtime +30 -exec rm {} \;
+
+    # Keep only the most recent sanitised backup.
+    find "$BACKUP_DIR" -name "*sanitised*-db.sqlite3.zst" ! -name "$SANITISED_BACKUP_FILEPATH.zst" -type f -exec rm {} \;
 }
 
 # log start of backup in a way that won't fail the whole script if sentry down

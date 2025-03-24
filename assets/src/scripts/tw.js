311fa4d4b0637eacbb3e0342db204f085b465857
Thomas O'Dwyer <thomas.odwyer@thedatalab.org>
2025-03-24 16:43:08 +0000

    Use a variable font

--- assets/src/scripts/tw.js
+++ assets/src/scripts/tw.js
@@ -1,8 +1,5 @@
 // Fonts
-import "@fontsource/public-sans/400.css";
-import "@fontsource/public-sans/500.css";
-import "@fontsource/public-sans/600.css";
-import "@fontsource/public-sans/700.css";
+import '@fontsource-variable/public-sans';
 
 // Web components
 import "@zachleat/details-utils";

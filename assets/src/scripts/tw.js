0095e5d3209c7b0a7fafa22548aead20328f7fa7
Thomas O'Dwyer <thomas.odwyer@thedatalab.org>
2025-03-24 15:08:14 +0000

    Set up navbar with details-utils

--- assets/src/scripts/tw.js
+++ assets/src/scripts/tw.js
@@ -1,6 +1,11 @@
+// Fonts
 import "@fontsource/public-sans/400.css";
 import "@fontsource/public-sans/500.css";
 import "@fontsource/public-sans/600.css";
 import "@fontsource/public-sans/700.css";
 
+// Web components
+import "@zachleat/details-utils";
+
+// Styles
 import "../styles/tw.css"

253216b2ff3cb476334428f1e065fc17d34819ca
Steven Maude <git@stevenmaude.co.uk>
2025-04-09 13:26:56 +0100

    Add fixture to run `playwright install`
    Reference:
    
    opensafely-core/airlock@039e1d148bcc5c4e9dad691bb92a97769f232ced

--- opencodelists/tests/functional/conftest.py
+++ opencodelists/tests/functional/conftest.py
@@ -1,4 +1,6 @@
 import os
+import subprocess
+import sys
 
 import pytest
 
@@ -8,3 +10,16 @@ def set_env():
     # This is required for playwright tests with Django
     # See https://github.com/microsoft/playwright-pytest/issues/29
     os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "1"
+
+
+@pytest.fixture(scope="session", autouse=True)
+def playwright_install(request):
+    # As this can potentially take a long time when it's first run (and as it is
+    # subsequently a silent no-op) we disable output capturing so that progress gets
+    # displayed to the user
+    capmanager = request.config.pluginmanager.getplugin("capturemanager")
+    command = [sys.executable, "-m", "playwright", "install"]
+    if os.environ.get("CI"):  # pragma: no cover
+        command.extend(["--with-deps", "chromium"])
+    with capmanager.global_and_fixture_disabled():
+        subprocess.run(command, check=True)

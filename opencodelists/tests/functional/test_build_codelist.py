0b1d8027cc3ead7c4e5fa67d923c8d684865b7a4
Steven Maude <git@stevenmaude.co.uk>
2025-04-10 18:10:30 +0100

    Add rudimentary commented-out test
    None of this works properly, and is a bit of a throwaway state
    ultimately.
    
    But this at least illustrates what Playwright can do with the builder, based
    on the codegen tool Playwright provides, and gets you to interacting
    with the builder if you run:
    
    ```
    just test-py --headed
    ```
    
    On my install at least, with this state, I can get to the builder and
    work on a test for the coding system builder (even if the test suite
    execution as a whole is all broken, because subsequent tests fail
    following the use of Playwright).

--- opencodelists/tests/functional/test_build_codelist.py
+++ opencodelists/tests/functional/test_build_codelist.py
@@ -4,9 +4,6 @@ from playwright.sync_api import expect
 from opencodelists.models import User
 
 
-pytestmark = pytest.mark.order("last")
-
-
 @pytest.fixture
 def functional_test_user():
     return User.objects.create_user(
@@ -34,7 +31,28 @@ def login_context(browser, user_cookies):
     return context
 
 
-def test_login_works(login_context, user_without_organisation, live_server):
+def test_codelist_builder(login_context, user_without_organisation, live_server):
     page = login_context.new_page()
     page.goto(live_server.url)
+    page.pause()
     expect(page.get_by_role("button", name="My account")).to_be_visible()
+
+
+# Rudimentary codegen coding system builder form usage:
+#    page.get_by_role("textbox", name="Codelist name*").fill("Test4")
+#    page.get_by_label("Coding system*").select_option("snomedct")
+#    page.get_by_role("button", name="Create").click()
+#    page.get_by_role("searchbox", name="Term or code").click()
+#    page.get_by_role("searchbox", name="Term or code").fill("arthritis")
+#    page.get_by_role("searchbox", name="Term or code").press("Enter")
+#    page.get_by_role("button", name="Search").click()
+#    page.get_by_role("button", name="⊞").click()
+#    page.get_by_role("button", name="+").first.click()
+#    page.get_by_role("button", name="−").nth(1).click()
+#    page.get_by_role("button", name="+").nth(2).click()
+#    page.get_by_role("searchbox", name="Term or code").click()
+#    page.get_by_role("searchbox", name="Term or code").fill("tennis")
+#    page.get_by_role("searchbox", name="Term or code").press("Enter")
+#    page.get_by_role("button", name="Search", exact=True).click()
+#    page.get_by_role("button", name="−").nth(2).click()
+#    page.get_by_role("button", name="Save draft").click()

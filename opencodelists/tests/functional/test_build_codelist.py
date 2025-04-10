07ee92931b6992e820c88e343bf7dde18b73e06c
Steven Maude <git@stevenmaude.co.uk>
2025-04-10 18:12:08 +0100

    Revert "Workaround for login not working"
    This reverts commit bd338fc4648b7ad76ceba446c3fefd65adf0a147.

--- opencodelists/tests/functional/test_build_codelist.py
+++ opencodelists/tests/functional/test_build_codelist.py
@@ -8,28 +8,33 @@ pytestmark = pytest.mark.order("last")
 
 
 @pytest.fixture
-def login_context(client, live_server, browser):
-    user = User.objects.create_user(
+def functional_test_user():
+    return User.objects.create_user(
         username="functional",
         password="test_user",
         email="functional_test_user@example.com",
         name="Functional Test User",
     )
-    user.save()
-    client.force_login(user)
-    user_cookies = {
+
+
+@pytest.fixture
+def user_cookies(client, functional_test_user, live_server):
+    client.force_login(functional_test_user)
+    return {
         "name": "sessionid",
         "value": client.cookies["sessionid"].value,
         "url": live_server.url,
     }
 
+
+@pytest.fixture
+def login_context(browser, user_cookies):
     context = browser.new_context()
     context.add_cookies([user_cookies])
     return context
 
 
-def test_login_works(login_context, live_server):
+def test_login_works(login_context, user_without_organisation, live_server):
     page = login_context.new_page()
     page.goto(live_server.url)
-    page.pause()
     expect(page.get_by_role("button", name="My account")).to_be_visible()

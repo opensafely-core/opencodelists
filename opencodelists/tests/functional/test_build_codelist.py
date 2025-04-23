8e47515a443743587280125abbe20d51c24adbdd
Steven Maude <git@stevenmaude.co.uk>
2025-04-23 23:17:45 +0100

    Get a rudimentary test working

--- opencodelists/tests/functional/test_build_codelist.py
+++ opencodelists/tests/functional/test_build_codelist.py
@@ -34,28 +34,48 @@ def login_context(browser, user_cookies):
     return context
 
 
+@pytest.mark.django_db(
+    databases=[
+        "default",
+        "snomedct_test_20200101",
+        "dmd_test_20200101",
+        "bnf_test_20200101",
+    ]
+)
 def test_codelist_builder(login_context, user_without_organisation, live_server):
     page = login_context.new_page()
     page.goto(live_server.url)
-    page.pause()
     expect(page.get_by_role("button", name="My account")).to_be_visible()
 
+    # Navigate to codelists.
+    page.get_by_role("link", name="My codelists").click()
+    page.get_by_role("link", name="Create a codelist!").click()
 
-# Rudimentary codegen coding system builder form usage:
-#    page.get_by_role("textbox", name="Codelist name*").fill("Test4")
-#    page.get_by_label("Coding system*").select_option("snomedct")
-#    page.get_by_role("button", name="Create").click()
-#    page.get_by_role("searchbox", name="Term or code").click()
-#    page.get_by_role("searchbox", name="Term or code").fill("arthritis")
-#    page.get_by_role("searchbox", name="Term or code").press("Enter")
-#    page.get_by_role("button", name="Search").click()
-#    page.get_by_role("button", name="⊞").click()
-#    page.get_by_role("button", name="+").first.click()
-#    page.get_by_role("button", name="−").nth(1).click()
-#    page.get_by_role("button", name="+").nth(2).click()
-#    page.get_by_role("searchbox", name="Term or code").click()
-#    page.get_by_role("searchbox", name="Term or code").fill("tennis")
-#    page.get_by_role("searchbox", name="Term or code").press("Enter")
-#    page.get_by_role("button", name="Search", exact=True).click()
-#    page.get_by_role("button", name="−").nth(2).click()
-#    page.get_by_role("button", name="Save draft").click()
+    # Complete the form to create a codelist.
+    page.get_by_role("textbox", name="Codelist name *").fill("Test")
+    page.get_by_label("Coding system *").select_option("snomedct")
+    page.get_by_role("button", name="Create").click()
+
+    # Make a search.
+    page.get_by_role("searchbox", name="Term or code").click()
+    page.get_by_role("searchbox", name="Term or code").fill("arthritis")
+    page.get_by_role("searchbox", name="Term or code").press("Enter")
+    page.get_by_role("button", name="Search", exact=True).click()
+
+    # Select some of the hierarchy.
+    page.get_by_role("button", name="⊞").click()
+    page.get_by_role("button", name="+").first.click()
+    page.get_by_role("button", name="−").nth(1).click()
+    page.get_by_role("button", name="+").nth(2).click()
+
+    # Make another search.
+    page.get_by_role("searchbox", name="Term or code").click()
+    page.get_by_role("searchbox", name="Term or code").fill("tennis")
+    page.get_by_role("searchbox", name="Term or code").press("Enter")
+    page.get_by_role("button", name="Search", exact=True).click()
+
+    # Select some of the hierarchy.
+    page.get_by_role("button", name="−").nth(2).click()
+
+    # Finish working on the codelist.
+    page.get_by_role("button", name="Save draft").click()

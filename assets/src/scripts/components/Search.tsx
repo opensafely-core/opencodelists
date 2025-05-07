ed2cff893e0c3d2dc22c7bbbe40c8596b4f225c0
Richard Williams <richard.williams@thedatalab.org>
2025-05-07 16:11:42 +0100

    Better styling after clicking previous search Without these changes, if you click on a previous search, it gains the "active" class and turns blue. This makes the button invisible until the page is refreshed by the completion of the GET request. Wiring it up like this ensures the Remove button style changes as soon as the link becomes active.

--- assets/src/scripts/components/Search.tsx
+++ assets/src/scripts/components/Search.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Card, Form, ListGroup } from "react-bootstrap";
 import { getCookie } from "../_utils";
 import { PageData } from "../types";
@@ -9,6 +9,17 @@ interface SearchProps {
 }
 
 export default function Search({ draftURL, searches }: SearchProps) {
+  const [activeUrl, setActiveUrl] = useState<string>(
+    () => searches.find((search) => search.active)?.url || "",
+  );
+
+  const handleClick = (url: string) => (e: React.MouseEvent) => {
+    // Don't trigger if clicking the Remove button
+    if (!(e.target as HTMLElement).closest("button")) {
+      setActiveUrl(url);
+    }
+  };
+
   return (
     <Card>
       <Card.Header as="h2" className="h6 font-weight-bold">
@@ -18,7 +29,12 @@ export default function Search({ draftURL, searches }: SearchProps) {
         {searches.map((search) => (
           <React.Fragment key={search.url}>
             {search.delete_url ? (
-              <ListGroup.Item action active={search.active} href={search.url}>
+              <ListGroup.Item
+                action
+                active={search.url === activeUrl}
+                href={search.url}
+                onClick={handleClick(search.url)}
+              >
                 <Form action={search.delete_url} method="post">
                   <Form.Control
                     name="csrfmiddlewaretoken"
@@ -32,7 +48,9 @@ export default function Search({ draftURL, searches }: SearchProps) {
                     name="delete-search"
                     type="submit"
                     size="sm"
-                    variant={search.active ? "light" : "outline-primary"}
+                    variant={
+                      search.url === activeUrl ? "light" : "outline-primary"
+                    }
                   >
                     Remove
                   </Button>
@@ -55,6 +73,7 @@ export default function Search({ draftURL, searches }: SearchProps) {
             action
             className="font-italic"
             href={encodeURI(draftURL)}
+            onClick={handleClick(draftURL)}
           >
             show all
           </ListGroup.Item>

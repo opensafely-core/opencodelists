213adcdd7ba48604856bdda111e5e81c9b3a7bed
Thomas O'Dwyer <thomas.odwyer@thedatalab.org>
2025-05-09 15:29:35 +0100

    Display tree data in a script tag on the template

--- codelists/views/version.py
+++ codelists/views/version.py
@@ -4,6 +4,7 @@ from django.utils.html import format_html
 
 from ..models import Status
 from ..presenters import present_search_results
+from ..tree_data import build_tree_data
 from .decorators import load_version
 
 
@@ -66,5 +67,10 @@ def version(request, clv):
         "search_results": present_search_results(clv, code_to_term),
         "user_can_edit": user_can_edit,
         "can_create_new_version": can_create_new_version,
+        "tree_data": build_tree_data(
+            child_map, code_to_term, code_to_status, tree_tables
+        )
+        if tree_tables
+        else None,
     }
     return render(request, "codelists/version.html", ctx)

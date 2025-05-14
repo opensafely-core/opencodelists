3e7d340ab2724f8270be8063a4fd2360f6118509
Steven Maude <git@stevenmaude.co.uk>
2025-05-14 19:24:42 +0100

    Stop tests from installing CI dependencies again
    See opensafely-core/airlock@e4ee6d286ffc9fe4c5bc9772e928559739d928d4.

--- opencodelists/tests/functional/conftest.py
+++ opencodelists/tests/functional/conftest.py
@@ -18,8 +18,10 @@ def playwright_install(request):
     # subsequently a silent no-op) we disable output capturing so that progress gets
     # displayed to the user
     capmanager = request.config.pluginmanager.getplugin("capturemanager")
-    command = [sys.executable, "-m", "playwright", "install"]
-    if os.environ.get("CI"):  # pragma: no cover
-        command.extend(["--with-deps", "chromium"])
+    command = [sys.executable, "-m", "playwright", "install", "chromium"]
+    # Install with dependencies in CI (but not in docker, as they've already
+    # been installed in the image)
+    if os.environ.get("CI") and not os.environ.get("DOCKER"):  # pragma: no cover
+        command.extend(["--with-deps"])
     with capmanager.global_and_fixture_disabled():
         subprocess.run(command, check=True)

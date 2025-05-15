58babb4e8c22c52b53d5c1f469faec7109a5ad15
Richard Williams <richard.williams@thedatalab.org>
2025-05-15 07:56:25 +0100

    Formatted tsx file

--- assets/src/scripts/components/EmptySearch.tsx
+++ assets/src/scripts/components/EmptySearch.tsx
@@ -8,15 +8,14 @@ export default function EmptyState() {
         <li>Check your spelling</li>
         <li>Try searching for a similar term</li>
         <li>
-            Ensure you have selected the correct option above the search box
-            ("Search by code" if searching by code rather than term)
+          Ensure you have selected the correct option above the search box
+          ("Search by code" if searching by code rather than term)
         </li>
       </ol>
       <p>
         If this search was an error, click "Remove" in the "Previous searches"
-        box on the left to delete it. If you'd like to record that you
-        performed this search but it returned no results, you can leave it as
-        is.
+        box on the left to delete it. If you'd like to record that you performed
+        this search but it returned no results, you can leave it as is.
       </p>
     </section>
   );

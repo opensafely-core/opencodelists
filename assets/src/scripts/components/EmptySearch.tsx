44c420220c000c8f9ae3efd18f553fe9e5389736
Richard Williams <rw251@users.noreply.github.com>
2025-05-15 07:52:42 +0100

    Update assets/src/scripts/components/EmptySearch.tsx
    Co-authored-by: Mike Kelly <mike.kelly@thedatalab.org>

--- assets/src/scripts/components/EmptySearch.tsx
+++ assets/src/scripts/components/EmptySearch.tsx
@@ -5,19 +5,18 @@ export default function EmptyState() {
     <section style={{ maxWidth: "80ch" }}>
       <p className="lead">Your search returned no concepts. You may want to:</p>
       <ol>
-        <li>Double check the spelling</li>
-        <li>
-          Make sure that if you're searching for a code (rather than a search
-          term) that you have selected the correct option ("Search by code"
-          above the search box on the left)
-        </li>
+        <li>Check your spelling</li>
         <li>Try searching for a similar term</li>
+        <li>
+            Ensure you have selected the correct option above the search box
+            ("Search by code" if searching by code rather than term)
+        </li>
       </ol>
       <p>
-        If this search was an error, you can click "Remove" from the "Previous
-        searches" box on the left to get rid of it. However, if you want other
-        people to know that you tried this search, but it returned no results,
-        then you can leave it as a blank search.
+        If this search was an error, click "Remove" in the "Previous searches"
+        box on the left to delete it. If you'd like to record that you
+        performed this search but it returned no results, you can leave it as
+        is.
       </p>
     </section>
   );

6b9f48c0f629f18e40a8bac7e3f6e9a4e184a5d6
Thomas O'Dwyer <thomas.odwyer@thedatalab.org>
2025-05-16 10:37:46 +0100

    Update app to display tree_data on Codelist page

---
+++ assets/src/scripts/tree/Container.tsx
@@ -0,0 +1,27 @@
+import slugify from "@sindresorhus/slugify";
+import React from "react";
+import { readValueFromPage } from "../_utils";
+import { CodeProps } from "./ListGroup";
+import Section from "./Section";
+
+interface SectionProps {
+  children: CodeProps[];
+  title: string;
+}
+
+export default function Container() {
+  const treeData = readValueFromPage("tree_data");
+
+  return (
+    <>
+      {treeData.map((section: SectionProps) => (
+        <Section
+          key={`section-${slugify(section.title)}`}
+          section={section}
+          slug={slugify(section.title)}
+          title={section.title}
+        />
+      ))}
+    </>
+  );
+}

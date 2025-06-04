ed4b62cf4ed2a733e876d4456f7f77dddf17c867
Katie Bickford <katie.bickford@thedatalab.org>
2025-06-04 12:03:11 +0100

    Update version card using existing data
    Co-authored-by: Thomas O'Dwyer <tomodwyer@users.noreply.github.com>

--- assets/src/scripts/components/Version.tsx
+++ assets/src/scripts/components/Version.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Badge, Card, ListGroup } from "react-bootstrap";
+import {
+  Badge,
+  Card,
+  ListGroup,
+  OverlayTrigger,
+  Tooltip,
+} from "react-bootstrap";
 import { VersionT } from "../types";
 
 interface VersionProps {
@@ -12,33 +18,46 @@ export default function Version({ versions }: VersionProps) {
       <Card.Header as="h2" className="h6 font-weight-bold">
         Versions
       </Card.Header>
-      <ListGroup variant="flush">
+      <ListGroup variant="flush" className="sidebar-versions" as="ol">
         {versions.map((version) => (
           <ListGroup.Item
-            action
-            active={version.current}
-            className="d-flex justify-content-between align-items-center"
-            disabled={version.current}
-            href={encodeURI(version.url)}
+            as="li"
             key={version.tag_or_hash}
-            variant={version.current ? "light" : undefined}
+            variant={version.current ? "secondary" : undefined}
           >
-            {version.tag_or_hash}
-            {version.status === "draft" ? (
-              <Badge className="ml-1" variant="light">
-                Draft
-              </Badge>
-            ) : null}
-            {version.status === "under review" ? (
-              <Badge className="ml-1" variant="info">
-                Under review
-              </Badge>
-            ) : null}
-            {version.status === "published" ? (
-              <Badge className="ml-1" variant="success">
-                Published
-              </Badge>
-            ) : null}
+            <div className="d-flex justify-content-between align-items-center">
+              <span className="d-block text-monospace font-weight-bold">
+                {version.current ? (
+                  version.tag_or_hash
+                ) : (
+                  <a href={encodeURI(version.url)}>{version.tag_or_hash}</a>
+                )}
+              </span>
+              {version.status === "draft" ? (
+                <Badge variant="light">Draft</Badge>
+              ) : null}
+              {version.status === "under review" ? (
+                <Badge variant="info">Under review</Badge>
+              ) : null}
+              {version.status === "published" ? (
+                <Badge variant="success">Published</Badge>
+              ) : null}
+            </div>
+            {/* <span className="created d-block p-0">
+              <OverlayTrigger
+                placement="right"
+                overlay={
+                  <Tooltip id={version.tag_or_hash}>
+                    When this codelist version was added to OpenCodelists
+                  </Tooltip>
+                }
+              >
+                <abbr className="font-weight-bold">Created</abbr>
+              </OverlayTrigger>
+              :{{ version.created_at | date:'d M Y' }}
+              at
+              {{ version.created_at | date:'H:i' }}
+            </span> */}
           </ListGroup.Item>
         ))}
       </ListGroup>

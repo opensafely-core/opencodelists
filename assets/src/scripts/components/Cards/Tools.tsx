ae338cb42e342aeb64d3e3cc28e7ab00a8a3b53b
Thomas O'Dwyer <thomas.odwyer@thedatalab.org>
2025-06-20 12:08:47 +0100

    Add a Tools component

---
+++ assets/src/scripts/components/Cards/Tools.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { Card, ListGroup } from "react-bootstrap";
+import { readValueFromPage } from "../../_utils";
+import listOfTools from "../../data/tools.json";
+
+const codingSystemNameMap: Record<string, string> = {
+  "Pseudo BNF": "bnf",
+  "CTV3 (Read V3)": "ctv3",
+  "Dictionary of Medicines and Devices": "dmd",
+  "ICD-10": "icd10",
+  "SNOMED CT (UK Clinical Edition)": "snomedct",
+};
+
+export default function Tools() {
+  const codingSystemName: string =
+    readValueFromPage("metadata")?.coding_system_name;
+
+  if (!codingSystemName) return null;
+
+  const [tools] = useState(() => {
+    return listOfTools.filter((tool) =>
+      tool.codingTypes.includes(codingSystemNameMap?.[codingSystemName]),
+    );
+  });
+
+  if (!tools.length) return null;
+
+  return (
+    <Card>
+      <Card.Header>
+        <h2 className="h6 font-weight-bold mb-1">Tools</h2>
+        <p className="small mb-0">
+          The following tools can help you to build a better and more accurate
+          codelist.
+        </p>
+      </Card.Header>
+      <ListGroup as="ul" variant="flush">
+        {tools.map((tool) => (
+          <ListGroup.Item key={tool.id} as="li">
+            <a
+              href={tool.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className=""
+            >
+              {tool.name}
+            </a>
+            {tool?.description && (
+              <p className="small mb-0">{tool.description}</p>
+            )}
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    </Card>
+  );
+}

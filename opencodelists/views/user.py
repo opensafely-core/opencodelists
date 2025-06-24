4113788167687083293e0fd52bd7038f1bb28c16
Richard Williams <richard.williams@thedatalab.org>
2025-06-24 15:28:46 +0100

    "my codelists" is now a searchable table
    - Previously the view returned 4 things: drafts, under_review, authored_for_organisation, and codelists. We have kept "codelists" as that is what is displayed on a user page that isn't the logged in user (`_that_user.html`). All the others have been replaced with a single `all_codelists` object which merges together all the codelists and versions a user would see in their table
    - Searching has been added via a text input box.  Given this allows you to quickly search by name, coding system, and status, I don't think we also need column sorting.

--- opencodelists/views/user.py
+++ opencodelists/views/user.py
@@ -13,10 +13,37 @@ def user(request, username):
         handles__is_current=True, versions__status=Status.PUBLISHED
     ).distinct()
 
-    # Find all of the codelists authored (but not owned) by this user with at least one published version.
-    authored_for_organisation = user.authored_for_organisation.distinct().difference(
-        owned_codelists
+    # All codelists authored by the user (either themselves or on behalf of an org)
+    # AND those with a draft or under review version owned by the user
+    codelists_to_display = list(
+        set(user.codelists.union(user.authored_for_organisation))
+        | {
+            version.codelist
+            for version in user.drafts.union(user.versions_under_review)
+        }
     )
+
+    # For each codelist we return it, and a list of versions including:
+    #   - all draft and under review versions
+    #   - the latest published version
+    all_codelists = [
+        {
+            "codelist": codelist,
+            "versions": list(
+                codelist.versions.exclude(status=Status.PUBLISHED).order_by("-id")
+            )
+            + (
+                [codelist.latest_published_version()]
+                if codelist.latest_published_version()
+                else []
+            ),
+        }
+        for codelist in sorted(codelists_to_display, key=lambda x: (x.name))
+        # We can't use a queryset order_by (where versions_under_review/drafts are querysets of CodelistVersion
+        # instances), as a codelist can have multiple versions and multiple handles, and this results in duplicates
+        # in the returned queryset. See https://code.djangoproject.com/ticket/18165
+    ]
+
     ctx = {
         "user": user,
         "codelists": [
@@ -25,24 +52,7 @@ def user(request, username):
         ],
         # note that name is a property on a codelist, not an attribute, and it comes from the current handle.
         # If we want to order codelists or versions by codelist name, we actually need to order them by handle name.
-        # We can't use a queryset order_by in the following cases (where versions_under_review/drafts are querysets of
-        # CodelistVersion instances), as a codelist can have multiple versions and multiple handles,
-        # and this results in duplicates in the returned queryset.
-        # See https://code.djangoproject.com/ticket/18165
-        # Sort by organisation then name
-        "authored_for_organisation": [
-            codelist.latest_published_version()
-            for codelist in sorted(
-                authored_for_organisation, key=lambda x: (x.owner.name, x.name)
-            )
-        ],
-        "under_review": sorted(
-            user.versions_under_review.select_related("codelist"),
-            key=lambda x: x.codelist.name,
-        ),
-        "drafts": sorted(
-            user.drafts.select_related("codelist"), key=lambda x: x.codelist.name
-        ),
+        "all_codelists": all_codelists,
     }
 
     if user == request.user:

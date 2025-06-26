2d76c6d67df6eecc154cda1261aeca54bebb7f49
Richard Williams <richard.williams@thedatalab.org>
2025-06-26 15:07:00 +0100

    Add endpoint for getting synonyms
    - Added under the "versioning" part of "coding_systems". This isn't related to versioning, but this is the place that exposes the existing single`/coding_systems` url. Having a url like `/coding_systems/synonyms/<coding_system>` made the most sense, so inserted here to avoid creating a whole new app.

--- coding_systems/versioning/views.py
+++ coding_systems/versioning/views.py
@@ -1,4 +1,8 @@
+import json
+
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 
 from codelists.coding_systems import CODING_SYSTEMS
 
@@ -14,3 +18,18 @@ def latest_releases(request):
         ]
     }
     return render(request, "versioning/latest_releases.html", ctx)
+
+
+@require_http_methods(["POST"])
+def lookup_synonyms(request, coding_system):
+    if coding_system not in CODING_SYSTEMS:
+        return JsonResponse({"error": "Invalid coding system"})
+
+    try:
+        data = json.loads(request.body)
+        codes = data.get("codes", [])
+    except Exception:
+        return JsonResponse({"error": "Badly formatted JSON request"})
+
+    cs = CODING_SYSTEMS[coding_system].get_by_release_or_most_recent()
+    return JsonResponse({"synonyms": cs.lookup_synonyms(codes)})

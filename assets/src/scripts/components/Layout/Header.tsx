0e286f057d4b7a12d3622f938175ab519b8f0d73
Thomas O'Dwyer <thomas.odwyer@thedatalab.org>
2025-07-15 11:36:24 +0100

    Replace Title and Metadata component in Header

--- assets/src/scripts/components/Layout/Header.tsx
+++ assets/src/scripts/components/Layout/Header.tsx
@@ -1,9 +1,8 @@
 import React from "react";
+import { Badge, ListGroup } from "react-bootstrap";
 import { PageData } from "../../types";
 import { SummaryProps } from "../Cards/Summary";
 import ManagementForm from "../ManagementForm";
-import Metadata from "../Metadata";
-import Title from "../Title";
 
 export default function Header({
   counts,
@@ -17,10 +16,48 @@ export default function Header({
   return (
     <div className="border-bottom mb-3 pb-3">
       <div className="d-flex flex-row justify-content-between gap-2 mb-2">
-        <Title name={metadata.codelist_name} />
+        <h1 className="h3">
+          {metadata.codelist_name}
+          <Badge className="align-text-bottom ml-1" variant="secondary">
+            Draft
+          </Badge>
+        </h1>
         {isEditable && <ManagementForm counts={counts} />}
       </div>
-      <Metadata data={metadata} />
+
+      <ListGroup horizontal className="small">
+        <ListGroup.Item className="py-1 px-2">
+          <dt>Coding system</dt>
+          <dd className="mb-0">{metadata.coding_system_name}</dd>
+        </ListGroup.Item>
+
+        <ListGroup.Item className="py-1 px-2">
+          <dt>Coding system release</dt>
+          <dd className="mb-0">
+            {metadata.coding_system_release.release_name}{" "}
+            {metadata.coding_system_release.valid_from ? (
+              <>({metadata.coding_system_release.valid_from})</>
+            ) : null}
+          </dd>
+        </ListGroup.Item>
+
+        {metadata.organisation_name ? (
+          <ListGroup.Item className="py-1 px-2">
+            <dt>Organisation</dt>
+            <dd className="mb-0">{metadata.organisation_name}</dd>
+          </ListGroup.Item>
+        ) : null}
+
+        <ListGroup.Item className="py-1 px-2">
+          <dt>Codelist ID</dt>
+          <dd className="mb-0">{metadata.codelist_full_slug}</dd>
+        </ListGroup.Item>
+
+        <ListGroup.Item className="py-1 px-2">
+          <dt>Version ID</dt>
+          <dd className="mb-0">{metadata.hash}</dd>
+        </ListGroup.Item>
+      </ListGroup>
     </div>
   );
 }

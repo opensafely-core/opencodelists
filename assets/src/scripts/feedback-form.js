b59918fccd93e6d8062b51486adb9ad9f0cdfeb0
Katie Bickford <katie.bickford@thedatalab.org>
2025-07-16 15:08:30 +0100

    Add feedback form to more templates
    Co-authored-by: Thomas O'Dwyer <tomodwyer@users.noreply.github.com>

--- assets/src/scripts/feedback-form.js
+++ assets/src/scripts/feedback-form.js
@@ -1,10 +1,3 @@
 import "@zachleat/details-utils";
 
 import "../styles/feedback-form.css";
-
-const feedbackForm = document.getElementById("feedbackForm");
-if (feedbackForm) {
-  // Example code
-  const summary = feedbackForm.querySelector("summary");
-  summary.classList.add("feedback-form__summary");
-}

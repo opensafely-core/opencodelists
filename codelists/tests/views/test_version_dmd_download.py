b4a152baa498cd8c064530f030f13907605e2f00
Jon Massey <jon.massey@thedatalab.org>
2025-07-18 13:40:54 +0100

    Revert "Use simpler approach in tests to get response text"

--- codelists/tests/views/test_version_dmd_download.py
+++ codelists/tests/views/test_version_dmd_download.py
@@ -1,4 +1,4 @@
 def test_get(client, bnf_data, bnf_version_asthma):
     rsp = client.get(bnf_version_asthma.get_dmd_download_url())
-    data = rsp.text
+    data = rsp.content.decode("utf8")
     assert data == bnf_version_asthma.dmd_csv_data_for_download()

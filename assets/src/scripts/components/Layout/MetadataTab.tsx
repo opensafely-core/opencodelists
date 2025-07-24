a77ebce3811d57f0e41e3eae45cc35ad1883756d
Thomas O'Dwyer <thomas.odwyer@thedatalab.org>
2025-07-24 14:16:10 +0100

    Create MetadataForm component and use in tab

--- assets/src/scripts/components/Layout/MetadataTab.tsx
+++ assets/src/scripts/components/Layout/MetadataTab.tsx
@@ -1,7 +1,10 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import { Form } from "react-bootstrap";
 import type { PageData, Reference } from "../../types";
 import type { MetadataFieldName } from "../CodelistBuilder";
+import MetadataForm from "../Metadata/MetadataForm";
 import ReferenceList from "../Metadata/ReferenceList";
 
 interface MetadataTabProps {
@@ -17,6 +20,8 @@ export default function MetadataTab({
   references,
   renderMetadataField,
 }: MetadataTabProps) {
+  const client = new QueryClient();
+
   return (
     <>
       <p className="font-italic">
@@ -24,9 +29,24 @@ export default function MetadataTab({
         build their codelist. Text added here will be ready for you to edit
         before you publish the codelist.
       </p>
+
+      <QueryClientProvider client={client}>
+        <div className="builder__metadata-forms">
+          <MetadataForm
+            id="description"
+            isEditable={isEditable}
+            name="Description"
+          />
+          <MetadataForm
+            id="methodology"
+            isEditable={isEditable}
+            name="Methodology"
+          />
+        </div>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+
       <Form noValidate>
-        {renderMetadataField("description")}
-        {renderMetadataField("methodology")}
         <ReferenceList
           isEditable={isEditable}
           references={references}
---
+++ assets/src/scripts/components/Metadata/MetadataForm.tsx
@@ -0,0 +1,127 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import React, { useState } from "react";
+import { postFetchWithOptions, readValueFromPage } from "../../_utils";
+import type { PageData } from "../../types";
+
+interface MetadataFieldProps {
+  id: string;
+  isEditable: PageData["isEditable"];
+  name: string;
+}
+
+export default function MetadataForm({
+  id,
+  isEditable,
+  name,
+}: MetadataFieldProps) {
+  const metadata = readValueFromPage("metadata");
+  const fieldMetadata = metadata[id];
+  const titleCaseName = name.charAt(0).toUpperCase() + name.slice(1);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const queryClient = useQueryClient();
+  const { data } = useQuery({
+    queryKey: ["metadata", id],
+    initialData: fieldMetadata,
+    queryFn: () => fieldMetadata,
+  });
+
+  const updateMetadata = useMutation({
+    mutationFn: async (body: Record<string, FormDataEntryValue>) => {
+      return postFetchWithOptions({
+        body,
+        url: readValueFromPage("update-url"),
+      });
+    },
+    onSuccess: (data) => {
+      queryClient.setQueryData(["metadata", id], data.metadata[id]);
+      setIsEditing(false);
+    },
+  });
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const formFieldData = Object.fromEntries(formData);
+    updateMetadata.mutate(formFieldData);
+  }
+
+  function handleKeyDown(e: React.KeyboardEvent<HTMLTextAreaElement>) {
+    // Handle Ctrl+Enter for Save
+    if (e.key === "Enter" && (e.ctrlKey || e.metaKey)) {
+      e.preventDefault();
+      (e.currentTarget.form as HTMLFormElement)?.requestSubmit();
+    }
+
+    // Handle Escape for Cancel
+    if (e.key === "Escape") {
+      e.preventDefault();
+      setIsEditing(false);
+    }
+  }
+
+  return (
+    <form
+      className="card"
+      onReset={() => setIsEditing(false)}
+      onSubmit={handleSubmit}
+    >
+      <div className="card-header d-flex flex-row align-items-center">
+        <h3 className="h5 mb-0 mr-auto">{titleCaseName}</h3>
+        {isEditable ? (
+          isEditing ? (
+            <>
+              <button className="btn btn-primary btn-sm" type="submit">
+                Save
+              </button>
+              <button className="btn btn-secondary btn-sm ml-1" type="reset">
+                Cancel
+              </button>
+            </>
+          ) : (
+            <button
+              className="btn btn-primary btn-sm"
+              onClick={() => setIsEditing(true)}
+              type="button"
+            >
+              Edit <span className="sr-only">{name}</span>
+            </button>
+          )
+        ) : null}
+      </div>
+
+      <div className="card-body">
+        {isEditing ? (
+          <div className="form-group">
+            <label className="form-label sr-only" htmlFor={`metadata-${id}`}>
+              {name}
+            </label>
+            <textarea
+              className="form-control"
+              defaultValue={data.text}
+              id={`metadata-${id}`}
+              name={id}
+              onKeyDown={handleKeyDown}
+              rows={5}
+            ></textarea>
+            <small className="form-text text-muted">
+              If you make changes, please remember to click Save (shortcut:
+              CTRL-ENTER) to keep them or Cancel (shortcut: ESC) to discard.
+            </small>
+          </div>
+        ) : (
+          <div
+            className="builder__markdown"
+            // biome-ignore lint/security/noDangerouslySetInnerHtml: backend is validating the markdown content
+            dangerouslySetInnerHTML={{
+              __html:
+                data.html ||
+                `<p class="text-muted">${titleCaseName} not provided</p>`,
+            }}
+          />
+        )}
+      </div>
+    </form>
+  );
+}

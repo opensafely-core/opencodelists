7ef80777175afd6de09868bf3dd0f686b527693c
Thomas O'Dwyer <thomas.odwyer@thedatalab.org>
2025-08-06 17:16:47 +0100

    Remove React Bootstrap from Header component

--- assets/src/scripts/components/Layout/Header.tsx
+++ assets/src/scripts/components/Layout/Header.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Badge, ListGroup } from "react-bootstrap";
 import type { PageData } from "../../types";
 import type { SummaryProps } from "../Cards/Summary";
 import ManagementForm from "../Header/ManagementForm";
@@ -18,20 +17,20 @@ export default function Header({
       <div className="d-flex flex-row justify-content-between gap-2 mb-2">
         <h1 className="h3">
           {metadata.codelist_name}
-          <Badge className="align-text-bottom ml-1" variant="secondary">
+          <span className="badge badge-secondary align-text-bottom ml-1">
             Draft
-          </Badge>
+          </span>
         </h1>
         {isEditable && <ManagementForm counts={counts} />}
       </div>
 
-      <ListGroup horizontal className="small">
-        <ListGroup.Item className="py-1 px-2">
+      <dl className="small list-group list-group-horizontal small">
+        <div className="list-group-item py-1 px-2">
           <dt>Coding system</dt>
           <dd className="mb-0">{metadata.coding_system_name}</dd>
-        </ListGroup.Item>
+        </div>
 
-        <ListGroup.Item className="py-1 px-2">
+        <div className="list-group-item py-1 px-2">
           <dt>Coding system release</dt>
           <dd className="mb-0">
             {metadata.coding_system_release.release_name}{" "}
@@ -39,25 +38,25 @@ export default function Header({
               <>({metadata.coding_system_release.valid_from})</>
             ) : null}
           </dd>
-        </ListGroup.Item>
+        </div>
 
         {metadata.organisation_name ? (
-          <ListGroup.Item className="py-1 px-2">
+          <div className="list-group-item py-1 px-2">
             <dt>Organisation</dt>
             <dd className="mb-0">{metadata.organisation_name}</dd>
-          </ListGroup.Item>
+          </div>
         ) : null}
 
-        <ListGroup.Item className="py-1 px-2">
+        <div className="list-group-item py-1 px-2">
           <dt>Codelist ID</dt>
           <dd className="mb-0">{metadata.codelist_full_slug}</dd>
-        </ListGroup.Item>
+        </div>
 
-        <ListGroup.Item className="py-1 px-2">
+        <div className="list-group-item py-1 px-2">
           <dt>Version ID</dt>
           <dd className="mb-0">{metadata.hash}</dd>
-        </ListGroup.Item>
-      </ListGroup>
+        </div>
+      </dl>
     </div>
   );
 }

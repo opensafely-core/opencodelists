ce63ff052cec565db6022b4dde8a2004a31acdf3
Richard Williams <richard.williams@thedatalab.org>
2025-08-06 17:00:24 +0100

    Handles missing codeset in definition download
    We hide the "Download definition" for codelists without a codeset, but it's still possible to manually create the URL. This then triggers an unhandled exception. This code handles it and returns a 400 to the user with an explanation.

--- codelists/views/version_download_definition.py
+++ codelists/views/version_download_definition.py
@@ -5,10 +5,18 @@ from .decorators import load_version
 
 @load_version
 def version_download_definition(request, clv):
-    response = HttpResponse(content_type="text/csv")
-    content_disposition = (
-        f'attachment; filename="{clv.download_filename()}-definition.csv"'
-    )
-    response["Content-Disposition"] = content_disposition
-    response.write(clv.definition_csv_data_for_download())
-    return response
+    try:
+        response = HttpResponse(content_type="text/csv")
+        content_disposition = (
+            f'attachment; filename="{clv.download_filename()}-definition.csv"'
+        )
+        response["Content-Disposition"] = content_disposition
+        response.write(clv.definition_csv_data_for_download())
+        return response
+    except ValueError as e:
+        if str(e) == "Codelist version does not have a codeset":
+            return HttpResponse(
+                "This codelist version does not have a codeset.",
+                status=400,
+            )
+        raise e

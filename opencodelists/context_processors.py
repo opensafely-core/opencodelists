4044abca85995341eea71fe495502e8d08afe0df
Steven Maude <git@stevenmaude.co.uk>
2025-08-13 17:45:33 +0100

    Add a context processor for taking screenshots
    The feedback form button ends up in screenshots, often as slightly
    awkward places (for example, in the middle of text), which may be due to
    the way Playwright views the page.
    
    Add a context processor so we can disable the button from appearing.
    This has to be independent from `IN_PRODUCTION` since we use
    `IN_PRODUCTION=True` in screenshots so that we don't get the indication bar
    at the top page that tells us we're in development mode.
    
    The alternative would be clicking to dismiss the button, or removing it
    from the DOM somehow, but this feels cleaner than adding the button to
    each test, and also makes the screenshot code more robust if the
    feedback form button is changed or removed in future.

--- opencodelists/context_processors.py
+++ opencodelists/context_processors.py
@@ -6,3 +6,10 @@ def in_production(request):
 
     As determined by settings.py / environment variables."""
     return {"in_production": settings.IN_PRODUCTION}
+
+
+def take_screenshots(request):
+    """Is the site being used to take screenshots?
+
+    As determined by settings.py / environment variables."""
+    return {"take_screenshots": settings.TAKE_SCREENSHOTS}
--- opencodelists/tests/test_context_processors.py
+++ opencodelists/tests/test_context_processors.py
@@ -1,5 +1,6 @@
 from opencodelists.context_processors import (
     in_production,
+    take_screenshots,
 )
 
 
@@ -17,3 +18,19 @@ class TestInProduction:
         settings.IN_PRODUCTION = True
         request = rf.get("/")
         assert in_production(request)["in_production"] is True
+
+
+class TestTakeScreenshots:
+    """Tests of the take_screenshots context processor."""
+
+    def test_not_taking_screenshots(self, rf, settings):
+        """Test when the site is not being used to take screenshots (TAKE_SCREENSHOTS=False)."""
+        settings.TAKE_SCREENSHOTS = False
+        request = rf.get("/")
+        assert take_screenshots(request)["take_screenshots"] is False
+
+    def test_taking_screenshots(self, rf, settings):
+        """Test when the site is being used to take screenshots (TAKE_SCREENSHOTS=True)."""
+        settings.TAKE_SCREENSHOTS = True
+        request = rf.get("/")
+        assert take_screenshots(request)["take_screenshots"] is True

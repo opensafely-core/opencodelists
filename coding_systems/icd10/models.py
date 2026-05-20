1b64e30747e061d00795c47721a8e4cbdb9042ad
Jon Massey <jon.massey@thedatalab.org>
2026-05-20 22:33:17 +0100

    Final model
    Removes transitional fields now that the data migration has moved their
    data to its new home.

--- coding_systems/icd10/models.py
+++ coding_systems/icd10/models.py
@@ -48,8 +48,6 @@ class Edition(models.Model):
 
 class Concept(models.Model):
     code = models.CharField(primary_key=True, max_length=7)
-    kind = models.CharField(max_length=len("category"))
-    term = models.CharField(max_length=200)
     parent = models.ForeignKey(
         "Concept", on_delete=models.CASCADE, related_name="children", null=True
     )

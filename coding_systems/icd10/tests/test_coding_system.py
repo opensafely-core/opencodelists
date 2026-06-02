20ff519754a4a0e1ccad059cbbd2be913ff92a74
Jon Massey <jon.massey@thedatalab.org>
2026-06-02 15:54:02 +0100

    Add missing ICD-10 search tests

--- coding_systems/icd10/tests/test_coding_system.py
+++ coding_systems/icd10/tests/test_coding_system.py
@@ -41,3 +41,29 @@ def test_code_to_term(icd10_data, coding_system):
 
 def test_matching_codes(icd10_data, coding_system):
     assert coding_system.matching_codes(["M77", "M770", "99999"]) == {"M77", "M770"}
+
+
+def test_search_by_term(icd10_data, coding_system):
+    assert coding_system.search_by_term("rickett") == {
+        "A77",
+        "A770",
+        "A771",
+        "A772",
+        "A773",
+    }
+
+
+def test_search_by_code_exact(icd10_data, coding_system):
+    assert coding_system.search_by_code("A77") == {"A77"}
+
+
+def test_search_by_code_wildcard(icd10_data, coding_system):
+    assert coding_system.search_by_code("A77*") == {
+        "A77",
+        "A770",
+        "A771",
+        "A772",
+        "A773",
+        "A778",
+        "A779",
+    }

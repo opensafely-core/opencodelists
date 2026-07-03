ec64de511b6a478dbbfecfb54feec44f4c9e3d27
Richard Williams <richard.williams@thedatalab.org>
2026-07-03 16:27:53 +0100

    Expose ICD-10 warning data to the frontend
    Pass icd10_term_differences and icd10_moved_codes to the front end for use in the warning banners. We do this in the builder (views.py->draft.html) and in the codelist viewer django templates (version.py->version.html).

--- builder/views.py
+++ builder/views.py
@@ -262,6 +262,17 @@ def _draft(request, draft, search_id):
         ],
     }
 
+    icd10_term_differences = (
+        coding_system.lookup_clinically_different_codes(codeset.all_codes())
+        if hasattr(coding_system, "lookup_clinically_different_codes")
+        else []
+    )
+    icd10_moved_codes = (
+        coding_system.lookup_moved_codes(codeset.all_codes())
+        if hasattr(coding_system, "lookup_moved_codes")
+        else []
+    )
+
     ctx = {
         "user": draft.author,
         "draft": draft,
@@ -286,6 +297,8 @@ def _draft(request, draft, search_id):
         "versions": versions,
         "metadata": metadata,
         "is_empty_codelist": is_empty_codelist,
+        "icd10_term_differences": icd10_term_differences,
+        "icd10_moved_codes": icd10_moved_codes,
     }
 
     return render(request, "builder/draft.html", ctx)

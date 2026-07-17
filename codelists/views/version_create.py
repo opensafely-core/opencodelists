6165c729563ea395202a577be230f73da235212b
Jon Massey <jon.massey@thedatalab.org>
2026-07-17 16:53:16 +0100

    Fix superfluous extra version

--- codelists/views/version_create.py
+++ codelists/views/version_create.py
@@ -2,6 +2,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.views.decorators.http import require_POST
 
+from codelists.models import Status
+
 from .. import actions
 from ..coding_systems import most_recent_database_alias
 from .decorators import load_version, require_permission
@@ -20,11 +22,13 @@ def version_create(request, version):
             version.coding_system.id
         )
     if not version.codelist.is_new_style():
-        version = actions.convert_codelist_to_new_style(codelist=version.codelist)
-
-    draft = actions.export_to_builder(
-        version=version,
-        author=request.user,
-        coding_system_database_alias=coding_system_database_alias,
-    )
+        draft = actions.convert_codelist_to_new_style(
+            codelist=version.codelist, status=Status.DRAFT
+        )
+    else:
+        draft = actions.export_to_builder(
+            version=version,
+            author=request.user,
+            coding_system_database_alias=coding_system_database_alias,
+        )
     return redirect(draft.get_builder_draft_url())

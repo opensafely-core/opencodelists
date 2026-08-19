0e68603122d59a398426463210e9f71c231b65bc
Jon Massey <jon.massey@thedatalab.org>
2026-08-19 15:37:16 +0100

    First draft email sender

---
+++ scripts/icd10_reports/email.py
@@ -0,0 +1,96 @@
+import csv
+import time
+from collections.abc import Generator
+from pathlib import Path
+
+import requests
+from django.conf import settings
+
+
+"""Taken from codelist rot notifier https://github.com/opensafely/dmd-codelist-report/blob/main/notifier/send_email.py"""
+
+FROM_ADDRESS = "OpenCodelists <no-reply@opencodelists.org>"
+REPLY_TO = "bennett@phc.ox.ac.uk"
+SEED_EMAILS = [
+    "jon.massey@thedatalab.org",
+    "jon.massey@phc.ox.ac.uk",
+]
+
+SUBJECT = "Your codelist {} requires attention"
+BODY = """
+Some plaintext here. Do we want to do an HTML email as well?
+If so we need to think about templating in the various bits.
+Not intractable by any means.
+"""
+
+
+def send_email(
+    to: str,
+    subject: str,
+    text: str | None = None,
+    html: str | None = None,
+    attachments: list[Path] | None = None,
+    only_seeds: bool = True,
+) -> tuple[int, str] | None:
+    data = {
+        "from": FROM_ADDRESS,
+        "to": to,
+        "subject": subject,
+        "text": text,
+        "html": html,
+        "h:Reply-To": REPLY_TO,
+    }
+    if only_seeds and to.lower() not in SEED_EMAILS:
+        return
+    attempts = 0
+    while True:
+        files = (
+            [("attachment", a.read_bytes()) for a in attachments] if attachments else []
+        )
+        response = requests.post(
+            settings.ANYMAIL["MAILGUN_API_URL"] + "messages",
+            auth=("api", settings.ANYMAIL["MAILGUN_API_KEY"]),
+            files=files,
+            data=data,
+        )
+        # 429 codes are either rate exceeded (2 types), or too large
+        # https://documentation.mailgun.com/en/latest/api-sending.html#rate-limits
+        # with message "<why> limit exceeded, try again" where why=[bytes|request|recipient]
+        if response.status_code == "429":
+            if response.text.lower().startswith("bytes"):
+                raise Exception("Email body or html too large")
+            else:
+                if attempts >= 5:
+                    raise Exception(
+                        f"Retry attempts exceeded.\nLast status {response.status_code}:{response.text}"
+                    )
+                attempts += 1
+                time.sleep(0.1)
+                continue
+        return (response.status_code, response.text)
+
+
+def check_status():
+    rq = requests.get(
+        settings.ANYMAIL["MAILGUN_API_URL"] + "events",
+        auth=("api", settings.ANYMAIL["MAILGUN_API_KEY"]),
+        params={"limit": 5},
+    )
+    return (rq.status_code, rq.text)
+
+
+def send_emails(
+    path_to_recipients_csv: Path, pdfs_dir: Path
+) -> Generator[tuple[str, str, tuple[int, str] | str]]:
+    recipients = list(csv.DictReader(path_to_recipients_csv.read_text()))
+    for recipient in recipients:
+        try:
+            resp = send_email(
+                to=recipient["email"],
+                subject=SUBJECT.format(recipient["codelist"]),
+                attachments=[pdfs_dir / recipient["pdf_filename"]],
+            )
+            if resp:
+                yield (recipient["email"], recipient["codelist"], resp)
+        except Exception as e:
+            yield (recipient["email"], recipient["codelist"], str(e))
